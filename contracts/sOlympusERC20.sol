13319107d0fd8b1ee5b3e86a65a9f2f3c55e2f21
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-07-09 07:21:44 -0400

    Update sOlympusERC20.sol

--- contracts/sOlympusERC20.sol
+++ contracts/sOlympusERC20.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at Etherscan.io on 2021-06-12
+*/
+
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.5;
 
@@ -988,26 +992,34 @@ contract Ownable is IOwnable {
 }
 
 interface IOracle {
-    function getPrice( address _pair ) external returns ( uint );
+    function getPrice( address _pool ) external returns ( uint );
 }
 
 contract sOlympus is ERC20Permit, Ownable {
 
+    /* ========== DEPENDENCIES ========== */
+
     using SafeMath for uint256;
+
+
+
+    /* ========== EVENTS ========== */
+
+    event LogSupply(uint256 indexed epoch, uint256 timestamp, uint256 totalSupply );
+    event LogRebase( uint256 indexed epoch, uint256 rebase, uint256 index );
+    event LogStakingContractUpdated( address stakingContract );
+
+
+
+    /* ========== MODIFIERS ========== */
 
     modifier onlyStakingContract() {
         require( msg.sender == stakingContract );
         _;
     }
 
-    address public stakingContract;
-    address public initializer;
 
-    IOracle oracle;
-    address pair;
-
-    event LogRebase( uint256 indexed epoch, uint256 totalSupply );
-    event LogStakingContractUpdated( address stakingContract );
+    /* ========== DATA STRUCTURES ========== */
 
     struct Rebase {
         uint epoch;
@@ -1019,14 +1031,24 @@ contract sOlympus is ERC20Permit, Ownable {
         uint indexAdjustedPrice;
         uint blockNumberOccured;
     }
-    Rebase[] public rebases;
 
-    uint public INDEX;
 
-    mapping( address => bool ) public cannotReceive;
+
+    /* ========== STATE VARIABLES ========== */
+
+    address initializer;
+
+    uint INDEX; // Index Gons - tracks rebase growth
+
+    address public stakingContract; // balance used to calc rebase
+
+    IOracle oracle; // pulls price from pool
+    address pool;
+
+    Rebase[] public rebases; // past rebase data    
 
     uint256 private constant MAX_UINT256 = ~uint256(0);
-    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 500000 * 10**9;
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 5000000 * 10**9;
 
     // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
     // Use the highest value that fits in a uint256 for max granularity.
@@ -1040,47 +1062,46 @@ contract sOlympus is ERC20Permit, Ownable {
 
     mapping ( address => mapping ( address => uint256 ) ) private _allowedValue;
 
+
+
+    /* ========== CONSTRUCTOR ========== */
+
     constructor() ERC20("Staked Olympus", "sOHM", 9) ERC20Permit() {
         initializer = msg.sender;
         _totalSupply = INITIAL_FRAGMENTS_SUPPLY;
         _gonsPerFragment = TOTAL_GONS.div(_totalSupply);
     }
 
-    function initialize( address stakingContract_ ) external returns ( bool ) {
+
+
+    /* ========== INITIALIZATION ========== */
+
+    function initialize( address stakingContract_ ) external {
         require( msg.sender == initializer );
+
         require( stakingContract_ != address(0) );
         stakingContract = stakingContract_;
         _gonBalances[ stakingContract ] = TOTAL_GONS;
-        
+
         emit Transfer( address(0x0), stakingContract, _totalSupply );
         emit LogStakingContractUpdated( stakingContract_ );
         
         initializer = address(0);
-        return true;
     }
 
-    function setIndex( uint _INDEX ) external onlyManager() returns ( bool ) {
+    function setIndex( uint _INDEX ) external onlyManager() {
         require( INDEX == 0 );
-        INDEX = _INDEX;
-        return true;
+        INDEX = gonsForBalance( _INDEX );
     }
 
-    function setOracle( address _oracle, address _pair ) external onlyManager() {
-        require( _oracle != address(0) );
+    function setPool( address _oracle, address _pool ) external onlyManager() {
         oracle = IOracle( _oracle );
-        require( _pair != address(0) );
-        pair = _pair;
+        pool = _pool;
     }
 
-    /**
-        @notice allows pools to be blocked from receiving, but not sending, sOHM
-        @param address_ address
-        @return bool
-     */
-    function toggleReceiver( address address_ ) external onlyManager() returns ( bool ) {
-        cannotReceive[ address_ ] = !cannotReceive[ address_ ];
-        return true;
-    }
+
+
+    /* ========== REBASE ========== */
 
     /**
         @notice increases sOHM supply to increase staking balances relative to profit_
@@ -1092,7 +1113,8 @@ contract sOlympus is ERC20Permit, Ownable {
         uint256 circulatingSupply_ = circulatingSupply();
 
         if ( profit_ == 0 ) {
-            emit LogRebase( block.timestamp, _totalSupply );
+            emit LogSupply( epoch_, block.timestamp, _totalSupply );
+            emit LogRebase( epoch_, 0, index() );
             return _totalSupply;
         } else if ( circulatingSupply_ > 0 ){
             rebaseAmount = profit_.mul( _totalSupply ).div( circulatingSupply_ );
@@ -1118,9 +1140,8 @@ contract sOlympus is ERC20Permit, Ownable {
         @param previousCirculating_ uint
         @param profit_ uint
         @param epoch_ uint
-        @return bool
      */
-    function _storeRebase( uint previousCirculating_, uint profit_, uint epoch_ ) internal returns ( bool ) {
+    function _storeRebase( uint previousCirculating_, uint profit_, uint epoch_ ) internal {
         uint rebasePercent = profit_.mul( 1e18 ).div( previousCirculating_ );
 
         rebases.push( Rebase ( {
@@ -1130,14 +1151,73 @@ contract sOlympus is ERC20Permit, Ownable {
             totalStakedAfter: circulatingSupply(),
             amountRebased: profit_,
             index: index(),
-            indexAdjustedPrice: index().mul( oracle.getPrice( pair ) ).div( 1e9 ),
+            indexAdjustedPrice: index().mul( oracle.getPrice( pool ) ).div( 1e9 ),
             blockNumberOccured: block.number
         }));
         
-        emit LogRebase( block.timestamp, _totalSupply );
+        emit LogSupply( epoch_, block.timestamp, _totalSupply );
+        emit LogRebase( epoch_, rebasePercent, index() );
+    }
 
+
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function transfer( address to, uint256 value ) public override returns (bool) {
+        uint256 gonValue = value.mul( _gonsPerFragment );
+        _gonBalances[ msg.sender ] = _gonBalances[ msg.sender ].sub( gonValue );
+        _gonBalances[ to ] = _gonBalances[ to ].add( gonValue );
+        emit Transfer( msg.sender, to, value );
         return true;
     }
+
+    function transferFrom( address from, address to, uint256 value ) public override returns (bool) {
+       _allowedValue[ from ][ msg.sender ] = _allowedValue[ from ][ msg.sender ].sub( value );
+       emit Approval( from, msg.sender,  _allowedValue[ from ][ msg.sender ] );
+
+        uint256 gonValue = gonsForBalance( value );
+        _gonBalances[ from ] = _gonBalances[from].sub( gonValue );
+        _gonBalances[ to ] = _gonBalances[to].add( gonValue );
+        emit Transfer( from, to, value );
+        return true;
+    }
+
+    function approve( address spender, uint256 value ) public override returns (bool) {
+         _allowedValue[ msg.sender ][ spender ] = value;
+         emit Approval( msg.sender, spender, value );
+         return true;
+    }
+
+    function increaseAllowance( address spender, uint256 addedValue ) public override returns (bool) {
+        _allowedValue[ msg.sender ][ spender ] = _allowedValue[ msg.sender ][ spender ].add( addedValue );
+        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
+        return true;
+    }
+
+    function decreaseAllowance( address spender, uint256 subtractedValue ) public override returns (bool) {
+        uint256 oldValue = _allowedValue[ msg.sender ][ spender ];
+        if (subtractedValue >= oldValue) {
+            _allowedValue[ msg.sender ][ spender ] = 0;
+        } else {
+            _allowedValue[ msg.sender ][ spender ] = oldValue.sub( subtractedValue );
+        }
+        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
+        return true;
+    }
+
+
+
+    /* ========== INTERNAL FUNCTIONS ========== */
+
+    // called in a permit
+    function _approve( address owner, address spender, uint256 value ) internal override virtual {
+        _allowedValue[owner][spender] = value;
+        emit Approval( owner, spender, value );
+    }
+
+
+
+    /* ========== VIEW FUNCTIONS ========== */
 
     function balanceOf( address who ) public view override returns ( uint256 ) {
         return _gonBalances[ who ].div( _gonsPerFragment );
@@ -1160,58 +1240,7 @@ contract sOlympus is ERC20Permit, Ownable {
         return balanceForGons( INDEX );
     }
 
-    function transfer( address to, uint256 value ) public override returns (bool) {
-        require( !cannotReceive[ to ], "Cannot send to this address" );
-        uint256 gonValue = value.mul( _gonsPerFragment );
-        _gonBalances[ msg.sender ] = _gonBalances[ msg.sender ].sub( gonValue );
-        _gonBalances[ to ] = _gonBalances[ to ].add( gonValue );
-        emit Transfer( msg.sender, to, value );
-        return true;
-    }
-
     function allowance( address owner_, address spender ) public view override returns ( uint256 ) {
         return _allowedValue[ owner_ ][ spender ];
-    }
-
-    function transferFrom( address from, address to, uint256 value ) public override returns ( bool ) {
-        require( !cannotReceive[ to ], "Cannot send to this address" );
-       _allowedValue[ from ][ msg.sender ] = _allowedValue[ from ][ msg.sender ].sub( value );
-       emit Approval( from, msg.sender,  _allowedValue[ from ][ msg.sender ] );
-
-        uint256 gonValue = gonsForBalance( value );
-        _gonBalances[ from ] = _gonBalances[from].sub( gonValue );
-        _gonBalances[ to ] = _gonBalances[to].add( gonValue );
-        emit Transfer( from, to, value );
-
-        return true;
-    }
-
-    function approve( address spender, uint256 value ) public override returns (bool) {
-         _allowedValue[ msg.sender ][ spender ] = value;
-         emit Approval( msg.sender, spender, value );
-         return true;
-    }
-
-    // What gets called in a permit
-    function _approve( address owner, address spender, uint256 value ) internal override virtual {
-        _allowedValue[owner][spender] = value;
-        emit Approval( owner, spender, value );
-    }
-
-    function increaseAllowance( address spender, uint256 addedValue ) public override returns (bool) {
-        _allowedValue[ msg.sender ][ spender ] = _allowedValue[ msg.sender ][ spender ].add( addedValue );
-        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
-        return true;
-    }
-
-    function decreaseAllowance( address spender, uint256 subtractedValue ) public override returns (bool) {
-        uint256 oldValue = _allowedValue[ msg.sender ][ spender ];
-        if (subtractedValue >= oldValue) {
-            _allowedValue[ msg.sender ][ spender ] = 0;
-        } else {
-            _allowedValue[ msg.sender ][ spender ] = oldValue.sub( subtractedValue );
-        }
-        emit Approval( msg.sender, spender, _allowedValue[ msg.sender ][ spender ] );
-        return true;
     }
 }
