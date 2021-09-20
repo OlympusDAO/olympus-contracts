9d5a837cb5ae8c6c8cecd2550b822fa6d4762b1a
sali <saadali93@gmail.com>
2021-09-20 15:42:16 -0500

    adding tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
   solidity: {
     compilers: [
--- test/TycheYieldDirector.test.js
+++ test/TycheYieldDirector.test.js
@@ -1,8 +1,7 @@
-const { ethers } = require("hardhat")
-const { expect, use } = require("chai");
-const { solidity} = require("ethereum-waffle");
+const { ethers, waffle } = require("hardhat")
+const { expect } = require("chai");
 
-use(solidity);
+const { provider, deployContract } = waffle;
 
 describe('TycheYieldDirector', async () => {
 
@@ -28,34 +27,37 @@ describe('TycheYieldDirector', async () => {
         user,
         admin,
         addr3,
-        owner,
-        ohmFactory,
-        ohm,
-        sOhmFactory,
-        sOhm,
-        stakingFactory,
-        staking,
-        stakingHelperFactory,
-        stakingHelper,
-        treasuryFactory,
-        treasury,
-        distributorFactory,
-        distributor
+        alice,
+        bob,
+        carol
+        //ohmFactory,
+        //ohm,
+        //sOhmFactory,
+        //sOhm,
+        //stakingFactory,
+        //staking,
+        //stakingHelperFactory,
+        //stakingHelper,
+        //treasuryFactory,
+        //treasury,
+        //distributorFactory,
+        //distributor
 
     before(async () => {
-        [deployer, user, admin, addr3] = await ethers.getSigners();
+        [deployer, alice, bob, carol] = await ethers.getSigners();
         //signers = await ethers.getSigners();
         //deployer = accounts[0];
         //user = accounts[1];
         //admin = accounts[2];
         //addr3 = accounts[3];
         
-        owner = await ethers.getSigner("0x763a641383007870ae96067818f1649e5586f6de")
+        //owner = await ethers.getSigner("0x763a641383007870ae96067818f1649e5586f6de")
 
         stakingFactory = await ethers.getContractFactory('OlympusStaking');
         ohmFactory = await ethers.getContractFactory('OlympusERC20Token');
         sOhmFactory = await ethers.getContractFactory('sOlympus');
         stakingHelperFactory = await ethers.getContractFactory('StakingHelper');
+        warmupFactory = await ethers.getContractFactory('StakingWarmup');
         treasuryFactory = await ethers.getContractFactory('OlympusTreasury');
         distributorFactory = await ethers.getContractFactory('Distributor');
         tycheFactory = await ethers.getContractFactory('TycheYieldDirector');
@@ -70,16 +72,22 @@ describe('TycheYieldDirector', async () => {
         sOhm = await sOhmFactory.deploy();
         staking = await stakingFactory.deploy(ohm.address, sOhm.address, "2200", "0", "1");
         stakingHelper = await stakingHelperFactory.deploy(staking.address, ohm.address);
-        //treasury = await treasuryFactory.deploy(
-        //  ohm.address,
-        //  "0x6b175474e89094c44da98b954eedeac495271d0f",
-        //  "0x34d7d7aaf50ad4944b70b320acb24c95fa2def7c",
-        //  "1"
-        //);
+        treasury = await treasuryFactory.deploy(
+          ohm.address,
+          "0x6b175474e89094c44da98b954eedeac495271d0f",
+          "0x34d7d7aaf50ad4944b70b320acb24c95fa2def7c",
+          "1"
+        );
         //distributor = await distributorFactory.deploy(treasury.address, ohm.address, "2200", "1");
         tyche = await tycheFactory.deploy(ohm.address, sOhm.address);
 
+        // Setup for each component
+        ohm.setVault(deployer.address)
         sOhm.initialize(staking.address);
+
+        // Set distributor, warmup, and locker
+        //staking.setContract("0", distributor.address);
+        //staking.setContract("1", warmup.address);
     });
 
     it('should set token addresses correctly', async () => {
@@ -90,8 +98,16 @@ describe('TycheYieldDirector', async () => {
     });
 
     it('should deposit tokens to recipient correctly', async () => {
-        await tyche.deposit("10", user.address);
+        await ohm.mint(deployer.address, "1000");
+        expect(await ohm.balanceOf(deployer.address)).to.equal(1000);
 
+        await ohm.approve(stakingHelper.address, "2000");
+
+        await stakingHelper.stake("1000");
+
+        //await tyche.deposit("10", bob.address);
+
+        //await expect(tyche.donationInfo[alice.address][0].recipient).is.equal(bob.address);
     });
  
 });
