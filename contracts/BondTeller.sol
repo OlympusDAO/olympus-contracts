6b6ea0bfcb569ae70b211393a93eb56911210931
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-09-26 01:11:04 -0400

    edits to bonds

--- contracts/BondTeller.sol
+++ contracts/BondTeller.sol
@@ -23,7 +23,7 @@ contract BondTeller {
     /* ========== EVENTS ========== */
 
     event BondCreated( address indexed bonder, uint payout, uint expires );
-    event BondRedeemed( address indexed bonder, uint payout, uint remaining );
+    event Redeemed( address indexed bonder, uint payout );
 
 
 
@@ -40,9 +40,9 @@ contract BondTeller {
 
     // Info for bond holder
     struct Bond {
-        uint payout; // OHM remaining to be paid
-        uint vesting; // Blocks left to vest
-        uint lastBlock; // Last interaction
+        uint payout; // sOHM remaining to be paid. agnostic balance
+        uint vested; // Block when vested
+        bool redeemed;
     }
 
 
@@ -51,9 +51,11 @@ contract BondTeller {
 
     address depository; // contract where users deposit bonds
     address immutable staking; // contract to stake payout
-    IERC20 immutable OHM; // payment token
+    IERC20 immutable OHM; 
+    IERC20 immutable sOHM; // payment token
+    ITreasury immutable treasury; 
 
-    mapping( address => Bond ) public bonderInfo; // user data
+    mapping( address => Bond[] ) public bonderInfo; // user data
 
 
 
@@ -64,13 +66,17 @@ contract BondTeller {
         depository = _depository;
         require( _staking != address(0) );
         staking = _staking;
+        require( _treasury != address(0) );
+        treasury = ITreasury( _treasury );
         require( _OHM != address(0) );
         OHM = IERC20( _OHM );
+        require( _sOHM != address(0) );
+        sOHM = _sOHM;
     }
 
 
 
-    /* ========== MUTABLE FUNCTIONS ========== */
+    /* ========== DEPOSITORY FUNCTIONS ========== */
 
     /**
      *  @notice add new bond payout to user data
@@ -78,52 +84,56 @@ contract BondTeller {
      *  @param _payout uint
      *  @param _end uint
      */
-    function newBond( address _bonder, uint _payout, uint _end ) external onlyDepository() {
-        OHM.safeTransferFrom( depository, address(this), _payout );
+    function newBond( address _bonder, uint _payout, uint _vesting ) external onlyDepository() {
+        treasury.mintRewards( address(this), _payout );
 
-        Bond memory info = bonderInfo[ _bonder ];
+        OHM.approve( staking, _payout ); // approve staking payout
 
-        uint newVesting = getNewVesting( info.vesting, _end, getPercentNew( info.payout, _payout ) );
-
-        bonderInfo[ _bonder ] = Bond({ 
-            payout: bonderInfo[ _bonder ].payout.add( _payout ),
-            vesting: newVesting,
-            lastBlock: block.number
-        });
+        // store bond & stake payout
+        bonderInfo[ _bonder ].push( Bond({ 
+            payout: IStaking( staking ).stake( _payout, address(this), true ),
+            vested: block.number.add( _vesting ),
+            redeemed: false
+        } ) );
 
         // indexed events are emitted
         emit BondCreated( _bonder, _payout, newVesting );
     }
 
+    /* ========== INTERACTABLE FUNCTIONS ========== */
+
+    /**
+     *  @notice redeems all redeemable bonds
+     *  @param _bonder address
+     *  @return uint
+     */
+    function redeemAll( address _bonder ) external returns ( uint ) {
+        return redeem( _bonder, indexesFor( _bonder ) );
+    }
+
     /** 
      *  @notice redeem bond for user
      *  @param _bonder address
-     *  @param _stake bool
+     *  @param _indexes uint[]
      *  @return uint
      */ 
-    function redeem( address _bonder, bool _stake ) external returns ( uint ) {
-        Bond memory info = bonderInfo[ _bonder ];
-        uint percentVested = percentVestedFor( _bonder ); // (blocks since last interaction / vesting term remaining)
+    function redeem( address _bonder, uint[] calldata indexes ) public returns ( uint ) {
+        uint dues;
+        for( uint i = 0; i < _indexes.length; i++ ) {
+            uint index = _indexes[ i ];
+            Bond memory info = bonderInfo[ _bonder ][ index ];
 
-        if ( percentVested >= 10000 ) { // if fully vested
-            delete bonderInfo[ _bonder ]; // delete user info
-            emit BondRedeemed( _bonder, info.payout, 0 ); // emit bond data
-
-            return pay( _bonder, info.payout, _stake );
-        } else { // if unfinished
-            // calculate payout vested
-            uint payout = info.payout.mul( percentVested ).div( 10000 );
-
-            // store updated deposit info
-            bonderInfo[ _bonder ] = Bond({
-                payout: info.payout.sub( payout ),
-                vesting: info.vesting.sub( block.number.sub( info.lastBlock ) ),
-                lastBlock: block.number
-            });
-
-            emit BondRedeemed( _bonder, payout, bonderInfo[ _bonder ].payout );
-            return pay( _bonder, payout, _stake );
+            if ( !info.redeemed && percentVestedFor( _bonder, index ) >= 10000 ) {
+                bonderInfo[ _bonder ][ index ].redeemed = true; // mark as redeemed
+                dues = dues.add( info.payout );
+            }
         }
+
+        dues = IStaking( staking ).fromAgnosticAmount( dues );
+
+        emit Redeemed( _bonder, dues );
+        pay( _bonder, dues );
+        return dues;
     }
 
 
@@ -131,65 +141,31 @@ contract BondTeller {
     /* ========== INTERNAL FUNCTIONS ========== */
 
     /**
-     *  @notice allow user to stake payout automatically
+     *  @notice send payout
      *  @param _amount uint
-     *  @param _stake bool
      *  @return uint
      */
-    function pay( address _bonder, uint _amount, bool _stake ) internal returns ( uint ) {
-        if ( !_stake ) { // if user does not want to stake
-            OHM.transfer( _bonder, _amount ); // send payout
-        } else { // if user wants to stake
-            IERC20( OHM ).approve( staking, _amount );
-            IStaking( staking ).stake( _amount, _bonder );
-        }
-        return _amount;
-    }
-
-    /**
-     *  @notice get new bonds' percent of total payout 
-     *  @param _initial uint
-     *  @param _new uint
-     *  @return percent_ uint
-     */
-    function getPercentNew( uint _initial, uint _new ) internal pure returns ( uint percent_ ) {
-        percent_ = _new.mul( 1e9 ).div( _initial.add( _new ) );
-    }
-
-    /**
-     *  @notice get new vesting end block
-     *  @param _initial uint
-     *  @param _new uint
-     *  @param _percent uint
-     *  @return vesting_ uint
-     */
-    function getNewVesting( uint _initial, uint _new, uint _percent ) internal pure returns ( uint vesting_ ) {
-        uint difference = _new.sub( _initial );
-        vesting_ = _initial.add( difference.mul( _percent ).div( 1e9 ) );
+    function pay( address _bonder, uint _amount ) internal {
+        sOHM.transfer( _bonder, _amount );
     }
 
 
 
     /* ========== VIEW FUNCTIONS ========== */
 
-    // VESTING
-
     /**
-     *  @notice calculate how far into vesting a depositor is
-     *  @param _depositor address
-     *  @return percentVested_ uint
+     *  @notice returns indexes of live bonds
+     *  @param _bonder address
+     *  @return indexes_ uint
      */
-    function percentVestedFor( address _depositor ) public view returns ( uint percentVested_ ) {
-        Bond memory bond = bonderInfo[ _depositor ];
-        uint blocksSinceLast = block.number.sub( bond.lastBlock );
-
-        if ( bond.vesting > 0 ) {
-            percentVested_ = blocksSinceLast.mul( 10000 ).div( bond.vesting );
-        } else {
-            percentVested_ = 0;
+    function indexesFor( address _bonder ) public view returns ( uint[] indexes_ ) {
+        Bond[] memory info = bonderInfo[ _bonder ];
+        for( uint i = 0; i < info.length; i++ ) {
+            if( !info[ i ].redeemed ) {
+                indexes_.push( i );
+            }
         }
     }
-
 
     // PAYOUT
     
@@ -198,14 +174,67 @@ contract BondTeller {
      *  @param _depositor address
      *  @return pendingPayout_ uint
      */
-    function pendingPayoutFor( address _depositor ) external view returns ( uint pendingPayout_ ) {
-        uint percentVested = percentVestedFor( _depositor );
-        uint payout = bonderInfo[ _depositor ].payout;
+    function pendingFor( address _bonder, uint[] calldata _indexes ) external view returns ( uint pendingPayout_ ) {
+        for( uint i = 0; i < _indexes.length; i++ ) {
+            uint index = _indexes[ i ];
+            uint payout = bonderInfo[ _bonder ][ index ].payout;
 
-        if ( percentVested >= 10000 ) {
-            pendingPayout_ = payout;
-        } else {
-            pendingPayout_ = payout.mul( percentVested ).div( 10000 );
+            if ( percentVestedFor( _bonder, index ) >= 10000 ) {
+                pendingPayout_ = pendingPayout_.add( payout );
+            }
+        }
+        
+        pendingPayout_ = IStaking( staking ).fromAgnosticAmount( pendingPayout_ );
+    }
+
+    /**
+     *  @notice pending on all bonds
+     *  @param _bonder address
+     *  @return uint
+     */
+    function totalPendingFor( address _bonder ) external view returns ( uint ) {
+        return pendingPayoutFor( _bonder, indexesFor( _bonder ) );
+    }
+
+    /**
+     *  @notice pending payout for each outstanding bond
+     *  @param _bonder address
+     *  @return pending_ uint[]
+     */
+    function allPendingFor( address _bonder ) external view returns ( uint[] pending_ ) {
+        uint[] memory indexes = indexesFor( _bonder );
+
+        for( uint i = 0; i < indexes.length; i++ ) {
+            pending_.push( pendingFor( _bonder, indexes[i] ) );
+        }
+    }
+
+    // VESTING
+
+    /**
+     *  @notice calculate how far into vesting a depositor is
+     *  @param _depositor address
+     *  @return percentVested_ uint
+     */
+    function percentVestedFor( address _bonder, uint _index ) public view returns ( uint percentVested_ ) {
+        Bond memory bond = bonderInfo[ _bonder ][ _index ];
+
+        uint blocksRemaining = bond.vested.sub( bond.lastInteraction );
+        uint blocksSince = block.number.sub( bond.lastInteraction );
+
+        percentVested_ = blocksSince.mul( 10000 ).div( blocksRemaining );
+    }
+
+    /**
+     *  @notice vested percent for each outstanding bond
+     *  @param _bonder address
+     *  @return percents_ uint[]
+     */
+    function allPercentVestedFor( address _bonder ) external view returns ( uint[] percents_ ) {
+        uint[] memory indexes = indexesFor( _bonder );
+
+        for( uint i = 0; i < indexes.length; i++ ) {
+            percents_.push( percentVestedFor( _bonder, indexes[i] ) );
         }
     }
 }
