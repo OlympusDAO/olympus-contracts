c2913ba5fe8d80961b2e2f37cd7bf2be9c55c77e
ZayenX <84199924+lolchocotaco@users.noreply.github.com>
2021-10-13 23:26:57 -0700

    Version 2 governance (#12)
    * Adjusting Compound's governor bravo for Olympus. wsOHM will be the voting token, adjusting that to write checkpoints. Need to work it out more, but thinking to have quorum votes and proposal threshold to be determined at time of proposal, since wsOHM's supply will be changing
    * Updated event
    * Fixed conflicting compilier versions
    * Fixed MAX_PROPOSAL_THRESHOLDPERCENT from 100000 (10%) to 10000 (1%)
    * Removed unneeded contracts
    * Upon wrapping and unwrapping can specify which address you would like wsOHM to be minted to upon wrapping, or sOHM to be transfered to during burn and unwrapping of wsOHM
    * using number of wsOHM needed to quorum/thershold based off of % of sOHM circulating supply
    * adjusted governor alpha to be inline with current governor OHMega
    * Updated delegator constructor to match delegate contract intializer
    * timelock
    * Removed unused interface
    * Staking helper that goes from OHM to v2 wsOHM and vice versa
    
    Co-authored-by: JExtor <JeffreyJExtor@gmail.com>

---
+++ contracts/governnace/wsOHM.sol
@@ -0,0 +1,371 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.5;
+
+import "../libraries/SafeERC20.sol";
+import "../libraries/SafeMath.sol";
+import "../libraries/Address.sol";
+
+import "../interfaces/IERC20.sol";
+import "../interfaces/IsOHM.sol";
+import "../types/ERC20.sol";
+
+contract wOHM is IERC20 {
+    /* ========== DEPENDENCIES ========== */
+
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint;
+
+
+    /* ========== EVENTS ========== */
+
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+
+    /* ========== DATA STRUCTURES ========== */
+
+    /// @notice A checkpoint for marking number of votes from a given block
+    struct Checkpoint {
+        uint fromBlock;
+        uint votes;
+    }
+
+    /* ========== STATE VARIABLES ========== */    
+
+    string public constant name = "Wrapped sOHM";
+    string public constant symbol = "wsOHM";
+    uint8 public constant decimals = 18;
+    uint256 public override totalSupply;
+
+    address public immutable sOHM;
+
+    mapping (address => mapping (uint => Checkpoint)) public checkpoints;
+    mapping (address => uint) public numCheckpoints;
+    mapping (address => address) public delegates;
+    mapping (address => uint) private _balances;
+    mapping (address => mapping (address => uint)) private _allowances;
+
+    /* ========== CONSTRUCTOR ========== */
+
+    constructor( address _sOHM ) {
+        require( _sOHM != address(0) );
+        sOHM = _sOHM;
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) external override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) external override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20};
+     *
+     * Requirements:
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    /**
+     * @notice Delegate votes from `msg.sender` to `delegatee`
+     * @param delegatee The address to delegate votes to
+     */
+    function delegate(address delegatee) external {
+        return _delegate(msg.sender, delegatee);
+    }
+
+    /**
+        @notice wrap sOHM
+        @param _amount uint
+        @return uint
+     */
+    function wrap( uint _amount, address _recipient ) external returns ( uint ) {
+        IERC20( sOHM ).safeTransferFrom( msg.sender, address(this), _amount );
+        
+        uint value = sOHMTowOHM( _amount );
+        _mint( _recipient, value );
+        return value;
+    }
+
+    /**
+        @notice unwrap sOHM
+        @param _amount uint
+        @return uint
+     */
+    function unwrap( uint _amount, address _recipient ) external returns ( uint ) {
+        _burn( msg.sender, _amount );
+
+        uint value = wOHMTosOHM( _amount );
+        IERC20( sOHM ).safeTransfer( _recipient, value );
+        return value;
+    }
+
+    /**
+        @notice converts wOHM amount to sOHM
+        @param _amount uint
+        @return uint
+     */
+    function wOHMTosOHM( uint _amount ) public view returns ( uint ) {
+        return _amount.mul( IsOHM( sOHM ).index() ).div( 10 ** decimals );
+    }
+
+    /**
+        @notice converts sOHM amount to wOHM
+        @param _amount uint
+        @return uint
+     */
+    function sOHMTowOHM( uint _amount ) public view returns ( uint ) {
+        return _amount.mul( 10 ** decimals ).div( IsOHM( sOHM ).index() );
+    }
+
+    /* ========== VIEW FUNCTIONS ========== */
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) external view override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+     /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) external view override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @notice Gets the current votes balance for `account`
+     * @param account The address to get votes balance
+     * @return The number of current votes for `account`
+     */
+    function getCurrentVotes(address account) external view returns (uint) {
+        uint nCheckpoints = numCheckpoints[account];
+        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
+    }
+
+    /**
+     * @notice Determine the prior number of votes for an account as of a block number
+     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
+     * @param account The address of the account to check
+     * @param blockNumber The block number to get the vote balance at
+     * @return The number of votes the account had as of the given block
+     */
+    function getPriorVotes(address account, uint blockNumber) external view returns (uint) {
+        require(blockNumber < block.number, "wsOHM::getPriorVotes: not yet determined");
+
+        uint nCheckpoints = numCheckpoints[account];
+        if (nCheckpoints == 0) {
+            return 0;
+        }
+
+        // First check most recent balance
+        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
+            return checkpoints[account][nCheckpoints - 1].votes;
+        }
+
+        // Next check implicit zero balance
+        if (checkpoints[account][0].fromBlock > blockNumber) {
+            return 0;
+        }
+
+        uint lower = 0;
+        uint upper = nCheckpoints - 1;
+        while (upper > lower) {
+            uint center = upper - (upper - lower) / 2; // ceil, avoiding overflow
+            Checkpoint memory cp = checkpoints[account][center];
+            if (cp.fromBlock == blockNumber) {
+                return cp.votes;
+            } else if (cp.fromBlock < blockNumber) {
+                lower = center;
+            } else {
+                upper = center - 1;
+            }
+        }
+        return checkpoints[account][lower].votes;
+    }
+
+
+    /* ========== INTERNAL FUNCTIONS ========== */
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        totalSupply = totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+        totalSupply = totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     *
+     * This is internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _delegate(address delegator, address delegatee) internal {
+        address currentDelegate = delegates[delegator];
+        uint delegatorBalance = _balances[delegator];
+        delegates[delegator] = delegatee;
+
+        emit DelegateChanged(delegator, currentDelegate, delegatee);
+
+        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
+    }
+
+    function _moveDelegates(address srcRep, address dstRep, uint amount) internal {
+        if (srcRep != dstRep && amount > 0) {
+            if (srcRep != address(0)) {
+                uint srcRepNum = numCheckpoints[srcRep];
+                uint srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
+                uint srcRepNew = srcRepOld.sub(amount);
+                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
+            }
+
+            if (dstRep != address(0)) {
+                uint dstRepNum = numCheckpoints[dstRep];
+                uint dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
+                uint dstRepNew = dstRepOld.add(amount);
+                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
+            }
+        }
+    }
+
+    function _writeCheckpoint(address delegatee, uint nCheckpoints, uint oldVotes, uint newVotes) internal {
+      uint blockNumber = safe32(block.number, "wsOHM::_writeCheckpoint: block number exceeds 32 bits");
+
+      if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
+          checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
+      } else {
+          checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
+          numCheckpoints[delegatee] = nCheckpoints + 1;
+      }
+
+      emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be to transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal { 
+        _moveDelegates(from, to, amount);
+    }
+
+    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
+        require(n < 2**32, errorMessage);
+        return uint32(n);
+    }
+}
