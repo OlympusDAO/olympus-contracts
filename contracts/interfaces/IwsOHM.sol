d5ff7f59d3aa873344cfb9c9b46fef75bc3475ba
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-10-16 19:12:58 -0400

    Update IwsOHM.sol

--- contracts/interfaces/IwsOHM.sol
+++ contracts/interfaces/IwsOHM.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.5;
 
-interface IwsOHM {
+import "./IERC20.sol";
+
+interface IwsOHM is IERC20 {
     function wrap( uint _amount ) external returns ( uint );
 
     function unwrap( uint _amount ) external returns ( uint );
