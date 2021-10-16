f021230ee9e15cf4eb1f2e6a9058d676afa237e7
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-10-16 19:03:53 -0400

    Update IgOHM.sol

--- contracts/interfaces/IgOHM.sol
+++ contracts/interfaces/IgOHM.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.5;
 
-import "./interfaces/IERC20.sol";
+import "./IERC20.sol";
 
 interface IgOHM is IERC20 {
     function mint( address _to, uint _amount ) external;
