c45ee67884db8b1e12b30b4dc2d2971a7bd8411d
LolChocotaco <zayenx@gmail.com>
2021-10-21 18:15:32 -0700

    Adding testing foundations

---
+++ test/utils/impersonate_account.js
@@ -0,0 +1,8 @@
+const {network} = require("hardhat");
+
+module.exports = async (address) => {
+    return network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [address]
+    });
+}
