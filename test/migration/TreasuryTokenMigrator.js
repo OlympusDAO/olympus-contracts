4c147543e75cb82cb283a3d04f62c0eb1e03f9ec
LolChocotaco <zayenx@gmail.com>
2021-10-22 10:39:37 -0700

    creating contracts, before timesout on block mining

--- test/migration/TreasuryTokenMigrator.js
+++ test/migration/TreasuryTokenMigrator.js
@@ -1,3 +1,4 @@
+const { ethers } = require("hardhat");
 const dai_abi = require("../../abis/dai");
 const { expect } = require("../utils/test_env.js");
 const frax_abi = require("../../abis/frax");
@@ -22,25 +23,35 @@ const OLD_WSOHM_ADDRESS = process.env.OLD_WSOHM_ADDRESS;
 const SUSHI_ROUTER = process.env.SUSHI_ROUTER;
 const UNISWAP_ROUTER = process.env.UNISWAP_ROUTER;
 
-let user0, user1, user2, manager, old_treasury, dai, weth, frax, lusd;
 const tokenAddresses = [DAI, FRAX, WETH, LUSD];
 const reserveToken = [true, true, true, true];
-let TreasuryTokenMigrator, olympusTokenMigrator, Olympus, olympus, NewTreasury, newTreasury;
+const EPOCH_LEGNTH = 2200;
+
+// Some of these need to be global for the fast_track method
+// TODO(zx): maybe refactor this
+let deployer, user1, user2, manager, old_treasury, dai, weth, frax, lusd;
+let olympusTokenMigrator, ohm, sOhm, newTreasury, newStaking;
 
 describe("Treasury Token Migration", async () => {
     before(async () => {
         await fork_network();
 
-        [user0, user1, user2] = await ethers.getSigners();
+        [deployer, user1, user2] = await ethers.getSigners();
 
-        Olympus = await ethers.getContractFactory("OlympusERC20Token");
-        olympus = await Olympus.deploy();
+        let ohmContract = await ethers.getContractFactory("OlympusERC20Token");
+        ohm = await ohmContract.deploy();
 
-        NewTreasury = await ethers.getContractFactory("OlympusTreasury");
-        newTreasury = await NewTreasury.deploy(olympus.address, 10);
+        let sOhmContract = await ethers.getContractFactory("sOlympus");
+        sOhm = await sOhmContract.connect(deployer).deploy();
 
-        OlympusTokenMigrator = await ethers.getContractFactory("OlympusTokenMigrator");
-        olympusTokenMigrator = await OlympusTokenMigrator.deploy(
+        let newTreasuryContract = await ethers.getContractFactory("OlympusTreasury");
+        newTreasury = await newTreasuryContract.deploy(ohm.address, 10);
+
+        let newStakingContract = await ethers.getContractFactory("OlympusStaking");
+        newStaking = await newStakingContract.deploy(ohm.address, sOhm.address, EPOCH_LEGNTH, 0, 0);
+
+        let tokenMigratorContract = await ethers.getContractFactory("OlympusTokenMigrator");
+        olympusTokenMigrator = await tokenMigratorContract.deploy(
             OLD_OHM_ADDRESS,
             OLD_SOHM_ADDRESS,
             OLD_TREASURY_ADDRESS,
@@ -52,7 +63,7 @@ describe("Treasury Token Migration", async () => {
             0
         );
 
-        await user0.sendTransaction({
+        await deployer.sendTransaction({
             to: TREASURY_MANAGER,
             value: ethers.utils.parseEther("1"), // 1 ether
         });
@@ -72,50 +83,56 @@ describe("Treasury Token Migration", async () => {
 
         await old_treasury.connect(manager).queue(3, olympusTokenMigrator.address);
 
+        console.log("fastTrack 1300");
         await fastTrack(13000);
 
         await old_treasury.connect(manager).toggle(3, olympusTokenMigrator.address, olympusTokenMigrator.address);
 
-        await newTreasury.connect(user0).enableOnChainGovernance();
+        await newTreasury.connect(deployer).enableOnChainGovernance();
 
+        console.log("fastTrack1000");
         await fastTrack(1000);
 
-        await newTreasury.connect(user0).enableOnChainGovernance();
+        await newTreasury.connect(deployer).enableOnChainGovernance();
 
+        console.log("fastTrack4");
         await fastTrack(4, tokenAddresses);
 
-        await newTreasury.connect(user0).enable(0, olympusTokenMigrator.address, olympusTokenMigrator.address);
+        await newTreasury.connect(deployer).enable(0, olympusTokenMigrator.address, olympusTokenMigrator.address);
     });
 
     it("Should fail if sender is not DAO", async () => {
         await expect(olympusTokenMigrator.connect(user1).addTokens(tokenAddresses, reserveToken)).to.revertedWith(
             "Ownable: caller is not the owner"
         );
+        await expect(
+            olympusTokenMigrator.conntect(user1).migrateContracts("_newTreasury", "_newStaking", "_newOHM", "_newsOHM")
+        ).to.be.revertedWith("Ownable: caller is not the owner");
         // TODO (zx:) APparently anyone can call this..
         // await expect(olympusTokenMigrator.connect(user1).migrate()).to.revertedWith("Only DAO can call this function");
     });
 
     it("should fail if token is not equal to reserve token", async () => {
-        await expect(olympusTokenMigrator.connect(user0).addTokens(tokenAddresses, [true, true, true])).to.revertedWith(
-            "arrays length does not match"
-        );
+        await expect(
+            olympusTokenMigrator.connect(deployer).addTokens(tokenAddresses, [true, true, true])
+        ).to.revertedWith("arrays length does not match");
     });
 
     it("Should allow DAO add tokens", async () => {
-        await olympusTokenMigrator.connect(user0).addTokens(tokenAddresses, reserveToken);
+        await olympusTokenMigrator.connect(deployer).addTokens(tokenAddresses, reserveToken);
     });
 
     it("Should fail if new treasury is not vault", async () => {
-        await expect(olympusTokenMigrator.connect(user0).migrate()).to.revertedWith(
+        await expect(olympusTokenMigrator.connect(deployer).migrate()).to.revertedWith(
             "VaultOwned: caller is not the Vault"
         );
     });
 
     it("Should allow DAO migrate tokens", async () => {
-        await olympus.connect(user0).setVault(newTreasury.address);
+        await ohm.connect(deployer).setVault(newTreasury.address);
         await fastTrack(2);
 
-        await olympusTokenMigrator.connect(user0).migrate();
+        await olympusTokenMigrator.connect(deployer).migrate();
         console.log("Migration done!");
         await fastTrack(3);
     });
@@ -124,7 +141,7 @@ describe("Treasury Token Migration", async () => {
 async function fastTrack(loop, address = []) {
     if (loop === 4) {
         for (let i = 0; i < loop; i++) {
-            await newTreasury.connect(user0).enable(2, address[i], address[i]);
+            await newTreasury.connect(deployer).enable(2, address[i], address[i]);
         }
     } else if (loop === 1) {
         const count = loop + 3;
@@ -144,10 +161,10 @@ async function fastTrack(loop, address = []) {
         const contract_name = ["dai", "frax", "weth", "lusd"];
 
         for (let i = 0; i < count; i++) {
-            const bal_before_tx = await contract[i].connect(user0).balanceOf(OLD_TREASURY_ADDRESS);
+            const bal_before_tx = await contract[i].connect(deployer).balanceOf(OLD_TREASURY_ADDRESS);
             console.log(`old_treasury_${contract_name[i]}_bal_before_tx`, bal_before_tx.toString());
 
-            const bal_after_tx = await contract[i].connect(user0).balanceOf(newTreasury.address);
+            const bal_after_tx = await contract[i].connect(deployer).balanceOf(newTreasury.address);
             console.log(`new_treasury_${contract_name[i]}_bal_before_tx`, bal_after_tx.toString());
         }
     } else if (loop === 3) {
@@ -157,10 +174,10 @@ async function fastTrack(loop, address = []) {
         const contract_name = ["dai", "frax", "weth", "lusd"];
 
         for (let i = 0; i < count; i++) {
-            const bal_before_tx = await contract[i].connect(user0).balanceOf(OLD_TREASURY_ADDRESS);
+            const bal_before_tx = await contract[i].connect(deployer).balanceOf(OLD_TREASURY_ADDRESS);
             console.log(`old_treasury_${contract_name[i]}_bal_after_tx`, bal_before_tx.toString());
 
-            const bal_after_tx = await contract[i].connect(user0).balanceOf(newTreasury.address);
+            const bal_after_tx = await contract[i].connect(deployer).balanceOf(newTreasury.address);
             console.log(`new_treasury_${contract_name[i]}bal_after_tx`, bal_after_tx.toString());
         }
     } else {
