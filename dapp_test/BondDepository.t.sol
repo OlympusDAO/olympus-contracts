db9f390748c6d18ecc9ba6161bd5f74247d9e31c
kushal256 <kushal256@maybe.com>
2021-10-23 21:43:58 -0700

    wip

--- dapp_test/BondDepository.t.sol
+++ dapp_test/BondDepository.t.sol
@@ -6,19 +6,34 @@ import "../contracts/BondDepository.sol";
 import "../contracts/Staking.sol";
 import "../contracts/OlympusERC20.sol";
 import "../contracts/sOlympusERC20.sol";
+import "../contracts/StandardBondingCalculator.sol";
+import "../contracts/mocks/Frax.sol";
+
 contract BondDepositoryTest is DSTest {
+    
     OlympusBondDepository internal bondDepository;
     OlympusStaking internal staking;
+    OlympusBondingCalculator internal bondingCalculator;
+
     OlympusERC20Token internal ohm;
     sOlympus internal sohm;
 
+
     function setUp() public {
-        
+    
         ohm = new OlympusERC20Token();
         sohm = new sOlympus();
 
+        bondingCalculator = new OlympusBondingCalculator(address(ohm));
+
         staking = new OlympusStaking(address(ohm), address(sohm), 8, 0, 0);
-        bondDepository = new OlympusBondDepository(address(ohm), address(this));
+        bondDepository = new OlympusBondDepository(address(ohm), address(bondingCalculator));
+    }
+
+    function test_erc20() public {
+        FRAX fraxToken = new FRAX();
+        bondDepository.addBond(fraxToken, bondingCalculator, 999999, false);
+
     }
 
  
