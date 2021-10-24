4fc00a0636af1907a5465c1c705016d6db261469
kushal256 <kushal256@maybe.com>
2021-10-24 13:42:21 -0700

    wip

--- dapp_test/BondDepository.t.sol
+++ dapp_test/BondDepository.t.sol
@@ -9,33 +9,36 @@ import "../contracts/Staking.sol";
 import "../contracts/OlympusERC20.sol";
 import "../contracts/sOlympusERC20.sol";
 import "../contracts/StandardBondingCalculator.sol";
+import "@uniswap/v2-core/contracts/UniswapV2Pair.sol";
 
 contract BondDepositoryTest is DSTest {
-  OlympusBondDepository internal bondDepository;
-  OlympusStaking internal staking;
-  OlympusBondingCalculator internal bondingCalculator;
+    OlympusBondDepository internal bondDepository;
+    OlympusStaking internal staking;
+    OlympusBondingCalculator internal bondingCalculator;
 
-  OlympusERC20Token internal ohm;
-  sOlympus internal sohm;
+    OlympusERC20Token internal ohm;
+    sOlympus internal sohm;
 
-  function setUp() public {
-    ohm = new OlympusERC20Token();
-    sohm = new sOlympus();
+    function setUp() public {
+        ohm = new OlympusERC20Token();
+        sohm = new sOlympus();
 
-    bondingCalculator = new OlympusBondingCalculator(address(ohm));
+        bondingCalculator = new OlympusBondingCalculator(address(ohm));
 
-    staking = new OlympusStaking(address(ohm), address(sohm), 8, 0, 0);
-    bondDepository = new OlympusBondDepository(address(ohm), address(bondingCalculator));
-  }
+        staking = new OlympusStaking(address(ohm), address(sohm), 8, 0, 0);
+        bondDepository = new OlympusBondDepository(address(ohm), address(bondingCalculator));
+    }
 
-  function test_createBond_deposit() public {
-    IUniswapV2Pair token = new IUniswapV2Pair("MIM");
-   
-    uint256 bondId = bondDepository.addBond(address(token), address(bondingCalculator), 999999, false);
-    bondDepository.setTerms(bondId, 2, false, 5, 6, 6, 10, 10, 10, 0);
+    function test_createBond_deposit() public {
 
-    address depositor = address(0x1);
-    address feo = address(0x2);
-    bondDepository.deposit(5, 200, depositor, bondId, feo);
-  }
+        UniswapV2Pair pair = new UniswapV2Pair();
+        pair.initialize(address(0x1), address(0x2));
+
+        uint256 bondId = bondDepository.addBond(address(pair), address(bondingCalculator), 999999, false);
+        bondDepository.setTerms(bondId, 2, false, 5, 6, 6, 10, 10, 10, 0);
+
+        address depositor = address(0x1);
+        address feo = address(0x2);
+        bondDepository.deposit(5, 200, depositor, bondId, feo);
+    }
 }
