a0aa2b3b9f963861e444e170f81a1e5a5d2ee3f4
tp <tonypianonft@gmail.com>
2021-10-25 20:36:27 -0500

    Address PR feedback
    * make more magic numbers constants
    * strengthen the "do nothing" assertions
    * update no recipients test to show it advances the epoch

--- test/staking/StakingDistributorTest.js
+++ test/staking/StakingDistributorTest.js
@@ -14,6 +14,7 @@ describe("Distributor", () => {
   let OHM;
   let treasury;
   let DistributorContract;
+  const epochLength = 2200;
 
   beforeEach(async () => {
     [owner, staking, other, guardian] = await ethers.getSigners();
@@ -25,19 +26,22 @@ describe("Distributor", () => {
 
   describe("constructor", () => {
     it("constructs correctly", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 1);
+      const nextEpochBlock = 1;
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, nextEpochBlock);
 
-      expect(await distributor.epochLength()).to.equal(2200);
-      expect(await distributor.nextEpochBlock()).to.equal(1);
+      expect(await distributor.epochLength()).to.equal(epochLength);
+      expect(await distributor.nextEpochBlock()).to.equal(nextEpochBlock);
     });
 
     it("does not accept 0x0 as OHM address", async () => {
-      await expect(DistributorContract.connect(owner).deploy(treasury.address, blackhole, 2200, 1)).
+      const nextEpochBlock = 1;
+      await expect(DistributorContract.connect(owner).deploy(treasury.address, blackhole, epochLength, nextEpochBlock)).
         to.be.reverted;
     });
 
     it("does not accept 0x0 as treasury address", async () => {
-      await expect(DistributorContract.connect(owner).deploy(blackhole, OHM.address, 2200, 1)).
+      const nextEpochBlock = 1;
+      await expect(DistributorContract.connect(owner).deploy(blackhole, OHM.address, epochLength, nextEpochBlock)).
         to.be.reverted;
     });
   });
@@ -45,27 +49,35 @@ describe("Distributor", () => {
   describe("distribute", () => {
     it("will do nothing if epoch is not complete", async () => {
       let currentBlock = await ethers.provider.send("eth_blockNumber");
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock).add(1));
+      // each await is a transaction in its own block, so we need to be 3
+      // blocks ahead to not rebase
+      let nextEpochBlock = BigNumber.from(currentBlock).add(3);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, nextEpochBlock);
 
       await distributor.distribute();
+
+      expect(BigNumber.from(await distributor.nextEpochBlock())).
+        to.equal(nextEpochBlock);
     });
 
-    it("will do nothing if there are no recipients", async () => {
+    it("only advances the epoch if there are no recipients", async () => {
       let currentBlock = await ethers.provider.send("eth_blockNumber");
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
 
       await distributor.distribute();
+
+      expect(await distributor.nextEpochBlock()).to.equal(BigNumber.from(currentBlock).add(epochLength));
     });
 
     it("mint from treasury and distribute to recipients", async () => {
       let currentBlock = await ethers.provider.send("eth_blockNumber");
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
       await distributor.addRecipient(staking.address, 2975);
       await distributor.addRecipient(other.address, 1521);
 
       await OHM.mock.totalSupply.returns(1000000);
-      await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
-      await treasury.mock.mintRewards.withArgs(other.address, 1521).returns();
+      await treasury.mock.mint.withArgs(staking.address, 2975).returns();
+      await treasury.mock.mint.withArgs(other.address, 1521).returns();
       await distributor.distribute();
 
       let info = await distributor.info(0);
@@ -75,12 +87,16 @@ describe("Distributor", () => {
     describe("rate adjustments", () => {
       it("can decrease distribution rate", async () => {
         let currentBlock = await ethers.provider.send("eth_blockNumber");
-        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
         await distributor.addRecipient(staking.address, 2975);
-        await distributor.setAdjustment(0, false, 5, 2000);
+        const index = 0;
+        const add = false;
+        const rate = 5;
+        const target = 2000;
+        await distributor.setAdjustment(index, add, rate, target);
 
         await OHM.mock.totalSupply.returns(1000000);
-        await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
+        await treasury.mock.mint.withArgs(staking.address, 2975).returns();
         await distributor.distribute();
 
         let info = await distributor.info(0);
@@ -89,12 +105,16 @@ describe("Distributor", () => {
 
       it("can increase distribution rate", async () => {
         let currentBlock = await ethers.provider.send("eth_blockNumber");
-        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
         await distributor.addRecipient(staking.address, 2975);
-        await distributor.setAdjustment(0, true, 5, 3000);
+        const index = 0;
+        const add = true;
+        const rate = 5;
+        const target = 3000;
+        await distributor.setAdjustment(index, add, rate, target);
 
         await OHM.mock.totalSupply.returns(1000000);
-        await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
+        await treasury.mock.mint.withArgs(staking.address, 2975).returns();
         await distributor.distribute();
 
         let info = await distributor.info(0);
@@ -103,12 +123,16 @@ describe("Distributor", () => {
 
       it("will not adjust if adjustment rate is zero", async () => {
         let currentBlock = await ethers.provider.send("eth_blockNumber");
-        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
         await distributor.addRecipient(staking.address, 2975);
-        await distributor.setAdjustment(0, false, 0, 2975);
+        const index = 0;
+        const add = false;
+        const rate = 0;
+        const target = 2975;
+        await distributor.setAdjustment(index, add, rate, target);
 
         await OHM.mock.totalSupply.returns(1000000);
-        await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
+        await treasury.mock.mint.withArgs(staking.address, 2975).returns();
         await distributor.distribute();
 
         let info = await distributor.info(0);
@@ -117,12 +141,16 @@ describe("Distributor", () => {
 
       it("will stop decreasing once target it met", async () => {
         let currentBlock = await ethers.provider.send("eth_blockNumber");
-        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
         await distributor.addRecipient(staking.address, 2975);
-        await distributor.setAdjustment(0, false, 5, 2970);
+        const index = 0;
+        const add = false;
+        const rate = 5;
+        const target = 2970;
+        await distributor.setAdjustment(index, add, rate, target);
 
         await OHM.mock.totalSupply.returns(1000000);
-        await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
+        await treasury.mock.mint.withArgs(staking.address, 2975).returns();
         await distributor.distribute();
 
         let info = await distributor.info(0);
@@ -133,12 +161,16 @@ describe("Distributor", () => {
 
       it("will stop increasing once target it met", async () => {
         let currentBlock = await ethers.provider.send("eth_blockNumber");
-        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, BigNumber.from(currentBlock));
+        let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, BigNumber.from(currentBlock));
         await distributor.addRecipient(staking.address, 2975);
-        await distributor.setAdjustment(0, true, 5, 2980);
+        const index = 0;
+        const add = true;
+        const rate = 5;
+        const target = 2980;
+        await distributor.setAdjustment(index, add, rate, target);
 
         await OHM.mock.totalSupply.returns(1000000);
-        await treasury.mock.mintRewards.withArgs(staking.address, 2975).returns();
+        await treasury.mock.mint.withArgs(staking.address, 2975).returns();
         await distributor.distribute();
 
         let info = await distributor.info(0);
@@ -151,19 +183,23 @@ describe("Distributor", () => {
 
   describe("setAdjustment", () => {
     it("sets the adjustment at the given index", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).addRecipient(staking.address, 2975);
 
-      await distributor.connect(owner).setAdjustment(0, false, 5, 2000);
+      const index = 0;
+      const add = true;
+      const rate = 5;
+      const target = 2000;
+      await distributor.connect(owner).setAdjustment(index, add, rate, target);
 
-      let adjustment = await distributor.adjustments(0);
-      expect(adjustment.add).to.equal(false);
-      expect(adjustment.rate).to.equal(5);
-      expect(adjustment.target).to.equal(2000);
+      let adjustment = await distributor.adjustments(index);
+      expect(adjustment.add).to.equal(add);
+      expect(adjustment.rate).to.equal(rate);
+      expect(adjustment.target).to.equal(target);
     });
 
     it("can only be done by governor or guardian", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).addRecipient(staking.address, 2975);
 
       await expect(distributor.connect(other).setAdjustment(0, false, 5, 2000)).
@@ -171,58 +207,69 @@ describe("Distributor", () => {
     });
 
     it("allows governor to make large adjustments", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).addRecipient(staking.address, 2975);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
-      await distributor.connect(owner).setAdjustment(0, false, 2975, 0);
+      const index = 0;
+      const add = false;
+      const rate = 2975;
+      const target = 0;
+      await distributor.connect(owner).setAdjustment(index, add, rate, target);
 
-      let adjustment = await distributor.adjustments(0);
-      expect(adjustment.add).to.equal(false);
-      expect(adjustment.rate).to.equal(2975);
-      expect(adjustment.target).to.equal(0);
+      let adjustment = await distributor.adjustments(index);
+      expect(adjustment.add).to.equal(add);
+      expect(adjustment.rate).to.equal(rate);
+      expect(adjustment.target).to.equal(target);
     });
 
     it("allows guardian to make adjustments up to 2.5%", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).addRecipient(staking.address, 1000);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
-      await distributor.connect(guardian).setAdjustment(0, false, 25, 0);
+      const index = 0;
+      const add = false;
+      const rate = 25;
+      const target = 0;
+      await distributor.connect(guardian).setAdjustment(index, add, rate, target);
 
-      let adjustment = await distributor.adjustments(0);
-      expect(adjustment.add).to.equal(false);
-      expect(adjustment.rate).to.equal(25);
-      expect(adjustment.target).to.equal(0);
+      let adjustment = await distributor.adjustments(index);
+      expect(adjustment.add).to.equal(add);
+      expect(adjustment.rate).to.equal(rate);
+      expect(adjustment.target).to.equal(target);
     });
 
     it("restricts guardian to from making adjustments over 2.5%", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).addRecipient(staking.address, 1000);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
-      await expect(distributor.connect(guardian).setAdjustment(0, false, 26, 0)).
+      const rate = 26
+      await expect(distributor.connect(guardian).setAdjustment(0, false, rate, 0)).
         to.be.revertedWith("Limiter: cannot adjust by >2.5%");
     });
   });
 
   describe("nextRewardAt", () => {
     it("returns the number of OHM to be distributed in the next epoch", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await OHM.mock.totalSupply.returns(3899568500546135);
 
-      let reward = await distributor.nextRewardAt(2975);
+      const rate = 2975
+      let reward = await distributor.nextRewardAt(rate);
       expect(reward).to.equal(11601216289124);
     });
   });
 
   describe("nextRewardFor", () => {
     it("returns the number of OHM to be distributed to the given address in the next epoch", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
-      await distributor.addRecipient(staking.address, 2975);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
+      const rate = 2975
+      await distributor.addRecipient(staking.address, rate);
       await OHM.mock.totalSupply.returns(3899568500546135);
 
       let reward = await distributor.nextRewardFor(staking.address);
@@ -230,7 +277,7 @@ describe("Distributor", () => {
     });
 
     it("returns the 0 if the address is not a recipient", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await OHM.mock.totalSupply.returns(3899568500546135);
 
       let reward = await distributor.nextRewardFor(other.address);
@@ -240,7 +287,7 @@ describe("Distributor", () => {
 
   describe("addRecipient", () => {
     it("will append a recipent to the list", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.addRecipient(staking.address, 2975);
       await distributor.addRecipient(other.address, 1000);
 
@@ -254,7 +301,7 @@ describe("Distributor", () => {
     });
 
     it("can only be done by governor", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
@@ -265,7 +312,7 @@ describe("Distributor", () => {
 
   describe("removeRecipeint", () => {
     it("will set reciepent and rate to 0", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.addRecipient(staking.address, 2975);
       await distributor.removeRecipient(0, staking.address);
 
@@ -275,13 +322,13 @@ describe("Distributor", () => {
     });
 
     it("will revert if address is incorrect", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.addRecipient(staking.address, 2975);
       await expect(distributor.removeRecipient(0, other.address)).to.be.reverted;
     });
 
     it("can be done by the guardian", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
@@ -294,7 +341,7 @@ describe("Distributor", () => {
     });
 
     it("must be done by either governor or guardian", async () => {
-      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, 2200, 0);
+      let distributor = await DistributorContract.connect(owner).deploy(treasury.address, OHM.address, epochLength, 0);
       await distributor.connect(owner).pushGuardian(guardian.address);
       await distributor.connect(guardian).pullGuardian();
 
