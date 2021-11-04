be789b5b59d7d53ba7cac9942122ff451bf28c65
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-11-04 12:00:25 -0400

    Update Guardable.sol

--- contracts/types/Guardable.sol
+++ contracts/types/Guardable.sol
@@ -29,12 +29,12 @@ contract Guardable is IGuardable {
     }
 
     function renounceGuardian() public virtual override onlyGuardian() {
-        emit GuardianPushed( _guardian, address(0) );
+        emit GuardianPulled( _guardian, address(0) );
         _guardian = address(0);
+        _newGuardian = address(0);
     }
 
     function pushGuardian( address newGuardian_ ) public virtual override onlyGuardian() {
-        require( newGuardian_ != address(0), "Guardable: new guardian is the zero address");
         emit GuardianPushed( _guardian, newGuardian_ );
         _newGuardian = newGuardian_;
     }
