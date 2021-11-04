90bfe3f1b336432de792eb027f16454e7bff76d3
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-11-04 15:42:25 -0400

    fix interfaces

--- contracts/BondTeller.sol
+++ contracts/BondTeller.sol
@@ -102,7 +102,8 @@ contract BondTeller is ITeller, Ownable {
         uint256 reward = _payout.mul(feReward).div(10_000);
         treasury.mint(address(this), _payout.add(reward));
 
-        OHM.approve(address(staking), _payout);
+        OHM.approve(address(staking), _payout); // approve staking payout
+
         staking.stake(address(this), _payout, true, true);
 
         FERs[_feo] = FERs[_feo].add(reward); // front end operator reward
