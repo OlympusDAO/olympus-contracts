4e64126e1549ae35566c4c354a4f6c859e00aaf2
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-11-04 15:36:27 -0400

    _to parameter on staking functions + migration output optionality
    specify _to recipient for staking functions, enabling them to be used as transfers
    
    allow migrator to choose output token

--- contracts/interfaces/IStaking.sol
+++ contracts/interfaces/IStaking.sol
@@ -2,28 +2,37 @@
 pragma solidity >=0.7.5;
 
 interface IStaking {
+    function stake(
+        address _to,
+        uint256 _amount,
+        bool _rebasing,
+        bool _claim
+    ) external returns (uint256);
 
-    function stake( address _to, uint _amount, bool _rebasing, bool _claim ) external returns ( uint );
+    function claim(address _recipient, bool _rebasing) external returns (uint256);
 
-    function claim ( address _recipient, bool _rebasing ) external returns ( uint );
-
-    function forfeit() external returns ( uint );
+    function forfeit() external returns (uint256);
 
     function toggleLock() external;
 
-    function unstake( address _to, uint _amount, bool _trigger, bool _rebasing ) external returns ( uint );
+    function unstake(
+        address _to,
+        uint256 _amount,
+        bool _trigger,
+        bool _rebasing
+    ) external returns (uint256);
 
-    function wrap( address _to, uint _amount ) external returns ( uint gBalance_ );
+    function wrap(address _to, uint256 _amount) external returns (uint256 gBalance_);
 
-    function unwrap( address _to, uint _amount ) external returns ( uint sBalance_ );
+    function unwrap(address _to, uint256 _amount) external returns (uint256 sBalance_);
 
     function rebase() external;
 
-    function index() external view returns ( uint );
+    function index() external view returns (uint256);
 
-    function contractBalance() external view returns ( uint );
+    function contractBalance() external view returns (uint256);
 
-    function totalStaked() external view returns ( uint );
+    function totalStaked() external view returns (uint256);
 
-    function supplyInWarmup() external view returns ( uint );
+    function supplyInWarmup() external view returns (uint256);
 }
