5b830d6b9adefae6eb0cf44963554a964ab34902
indigo <6822158+ind-igo@users.noreply.github.com>
2021-11-08 11:26:47 -0600

    clean up OHM erc20, inherit interface

--- contracts/interfaces/IOHMERC20.sol
+++ contracts/interfaces/IOHMERC20.sol
@@ -4,11 +4,9 @@ pragma solidity >=0.7.5;
 import "./IERC20.sol";
 
 interface IOHMERC20 is IERC20 {
-  function mint(uint256 amount_) external;
+  function mint(address account_, uint256 amount_) external;
 
-  function mint(address account_, uint256 ammount_) external;
+  function burn(uint256 amount) external;
 
   function burnFrom(address account_, uint256 amount_) external;
-
-  function vault() external returns (address);
 }
