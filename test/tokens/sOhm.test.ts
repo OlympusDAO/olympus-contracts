ca58fea53d3956c2fc30b3ad38622494cb2c1194
indigo <6822158+ind-igo@users.noreply.github.com>
2021-11-09 11:50:12 -0600

    fix sohm init issue

--- test/tokens/sOhm.test.ts
+++ test/tokens/sOhm.test.ts
@@ -16,6 +16,9 @@ import {
   GOHM__factory,
   OlympusStaking,
   OlympusStaking__factory,
+  OlympusTreasury,
+  OlympusTreasury__factory,
+  ITreasury,
 } from '../../types';
 
 const TOTAL_GONS = 5000000000000000;
@@ -29,10 +32,12 @@ describe("sOhm", () => {
   let sOhm: SOlympus;
   let gOhmFake: FakeContract<GOHM>;
   let stakingFake: FakeContract<IStaking>;
+  let treasuryFake: FakeContract<ITreasury>;
 
   beforeEach(async () => {
     [initializer, alice, bob] = await ethers.getSigners();
     stakingFake = await smock.fake<IStaking>('IStaking');
+    treasuryFake = await smock.fake<ITreasury>('ITreasury');
     gOhmFake = await smock.fake<GOHM>('gOHM');
     ohm = await (new OlympusERC20Token__factory(initializer)).deploy();
     sOhm = await (new SOlympus__factory(initializer)).deploy();
@@ -78,23 +83,23 @@ describe("sOhm", () => {
 
     describe("initialize", () => {
       it("assigns TOTAL_GONS to the stakingFake contract's balance", async () => {
-        await sOhm.connect(initializer).initialize(stakingFake.address);
+        await sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address);
         expect(await sOhm.balanceOf(stakingFake.address)).to.equal(TOTAL_GONS);
       });
 
       it("emits Transfer event", async () => {
-        await expect(sOhm.connect(initializer).initialize(stakingFake.address)).
+        await expect(sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address)).
           to.emit(sOhm, "Transfer").withArgs(ZERO_ADDRESS, stakingFake.address, TOTAL_GONS);
       });
 
       it("emits LogStakingContractUpdated event", async () => {
-        await expect(sOhm.connect(initializer).initialize(stakingFake.address)).
+        await expect(sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address)).
           to.emit(sOhm, "LogStakingContractUpdated").withArgs(stakingFake.address);
       });
 
       it("unsets the initializer, so it cannot be called again", async () => {
-        await sOhm.connect(initializer).initialize(stakingFake.address);
-        await expect(sOhm.connect(initializer).initialize(stakingFake.address)).to.be.reverted;
+        await sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address);
+        await expect(sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address)).to.be.reverted;
       });
     });
   });
@@ -103,7 +108,7 @@ describe("sOhm", () => {
     beforeEach(async () => {
       await sOhm.connect(initializer).setIndex(1);
       await sOhm.connect(initializer).setgOHM(gOhmFake.address);
-      await sOhm.connect(initializer).initialize(stakingFake.address);
+      await sOhm.connect(initializer).initialize(stakingFake.address, treasuryFake.address);
     });
 
     describe("approve", () => {
@@ -161,7 +166,7 @@ describe("sOhm", () => {
         await gOhmFake.totalSupply.returns(0);
         await gOhmFake.balanceFrom.returns(0);
 
-        let totalSupply = await sOhm.circulatingSupply();
+        const totalSupply = await sOhm.circulatingSupply();
         expect(totalSupply).to.equal(0);
       });
 
@@ -170,7 +175,7 @@ describe("sOhm", () => {
         await gOhmFake.totalSupply.returns(10);
         await gOhmFake.balanceFrom.returns(10);
 
-        let totalSupply = await sOhm.circulatingSupply();
+        const totalSupply = await sOhm.circulatingSupply();
         expect(totalSupply).to.equal(10);
       });
 
@@ -180,7 +185,7 @@ describe("sOhm", () => {
         await gOhmFake.totalSupply.returns(0);
         await gOhmFake.balanceFrom.returns(0);
 
-        let totalSupply = await sOhm.circulatingSupply();
+        const totalSupply = await sOhm.circulatingSupply();
         expect(totalSupply).to.equal(50);
       });
     });
