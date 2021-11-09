eeb37a45559fe446b9f051af26765e57ce218fd6
FinagleLord <homelessducks@gmail.com>
2021-11-08 21:14:29 -0600

    Access control modifier tests

--- test/dapp_test/src/OlympusAuthority.t.sol
+++ test/dapp_test/src/OlympusAuthority.t.sol
@@ -3,28 +3,84 @@ pragma solidity 0.7.5;
 
 import "ds-test/test.sol"; // ds-test
 import "../../../contracts/OlympusAuthority.sol";
-
+import "../../../contracts/mocks/AccessControlledMock.sol";
 
 contract OlympusAuthorityTest is DSTest {
 
     OlympusAuthority authority;
-    address dummy = address(0x1);
+    AccessControlledMock accessControlledMock;
 
-    function setUp() public {
-        authority = new OlympusAuthority(dummy, address(this), address(this), address(this));
-    }
+    address UNAUTHORIZED_USER = address(0x1);
 
-    function testCannot_governor() public {
 
-        authority.pushGuardian(address(0x0), true);
-        authority.pushPolicy(address(0x0), true);
-
-        try authority.pushGovernor(address(0x0), true) {
+    function test_onlyGovernor_not_authorized() public {
+        authority = new OlympusAuthority(UNAUTHORIZED_USER, address(this), address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        try accessControlledMock.governorTest() {
             fail();
         } catch Error(string memory error) {
-            // Assert revert error matches expected message
             assertEq("UNAUTHORIZED", error);
         }
+    }
+
+    function test_onlyGovernor_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        accessControlledMock.governorTest();
+    }
+
+
+
+    function test_onlyGuardian_not_authorized() public {
+        authority = new OlympusAuthority(address(this), UNAUTHORIZED_USER, address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        try accessControlledMock.guardianTest() {
+            fail();
+        } catch Error(string memory error) {
+            assertEq("UNAUTHORIZED", error);
+        }
+    }
+
+    function test_onlyGuardian_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        accessControlledMock.guardianTest();
+    }
+
+
+
+    function test_onlyPolicy_not_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), UNAUTHORIZED_USER, address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        try accessControlledMock.policyTest() {
+            fail();
+        } catch Error(string memory error) {
+            assertEq("UNAUTHORIZED", error);
+        }
+    }
+
+    function test_onlyPolicy_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        accessControlledMock.policyTest();
+    }
+
+
+
+    function test_onlyVault_not_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), address(this), UNAUTHORIZED_USER);
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        try accessControlledMock.vaultTest() {
+            fail();
+        } catch Error(string memory error) {
+            assertEq("UNAUTHORIZED", error);
+        }
+    }
+
+    function test_onlyVault_authorized() public {
+        authority = new OlympusAuthority(address(this), address(this), address(this), address(this));
+        accessControlledMock = new AccessControlledMock( address(authority) );
+        accessControlledMock.vaultTest();
     }
 
 
