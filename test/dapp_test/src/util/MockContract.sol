8dabdef6780a77f6dd879550c9aeed6e857be3be
kushal256 <kushal256@maybe.com>
2021-11-19 09:01:56 -0800

    cleanup

--- test/dapp_test/src/util/MockContract.sol
+++ test/dapp_test/src/util/MockContract.sol
@@ -370,8 +370,8 @@ contract MockContract is MockInterface {
 		}
 
 		// Record invocation as separate call so we don't rollback in case we are called with STATICCALL
-		// (, bytes memory r) = address(this).call{gas: 100000}(abi.encodeWithSignature("updateInvocationCount(bytes4,bytes)", methodId, msg.data));
-		// assert(r.length == 0);
+//		(, bytes memory r) = address(this).call{gas: 100000}(abi.encodeWithSignature("updateInvocationCount(bytes4,bytes)", methodId, msg.data));
+//		assert(r.length == 0);
 
 		assembly {
 			return(add(0x20, result), mload(result))
