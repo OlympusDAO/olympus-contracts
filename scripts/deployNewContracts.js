9d5992822223e67a71d0d4e0c26966441e6819c7
JExtor <JeffreyJExtor@gmail.com>
2021-11-22 13:58:01 -0500

    Fix typo

--- scripts/deployNewContracts.js
+++ scripts/deployNewContracts.js
@@ -9,22 +9,22 @@ async function main() {
     const firstEpochNumber = "";
     const firstBlockNumber = "";
     const gOHM = "";
-    const authroity = "";
+    const authority = "";
 
     const OHM = await ethers.getContractFactory('OlympusERC20Token');
-    const ohm = await OHM.deploy(authroity);
+    const ohm = await OHM.deploy(authority);
 
     const OlympusTreasury = await ethers.getContractFactory('OlympusTreasury');
-    const olympusTreasury = await OlympusTreasury.deploy(ohm.address, '0', authroity);
+    const olympusTreasury = await OlympusTreasury.deploy(ohm.address, '0', authority);
 
     const SOHM = await ethers.getContractFactory('sOlympus');
     const sOHM = await SOHM.deploy();
 
     const OlympusStaking = await ethers.getContractFactory('OlympusStaking');
-    const staking = await OlympusStaking.deploy(ohm.address, sOHM.address, gOHM, '2200', firstEpochNumber, firstBlockNumber, authroity);
+    const staking = await OlympusStaking.deploy(ohm.address, sOHM.address, gOHM, '2200', firstEpochNumber, firstBlockNumber, authority);
 
     const Distributor = await ethers.getContractFactory('Distributor');
-    const distributor = await Distributor.deploy(olympusTreasury.address, ohm.address, staking.address, authroity );
+    const distributor = await Distributor.deploy(olympusTreasury.address, ohm.address, staking.address, authority );
 
     await sOHM.setIndex('');
     await sOHM.setgOHM(gOHM);
