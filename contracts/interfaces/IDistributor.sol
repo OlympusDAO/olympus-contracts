aa71d7db8b66a52fc8ef24d73587edc9ba0b1f3d
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-12-01 21:10:25 -0500

    fix tests

--- contracts/interfaces/IDistributor.sol
+++ contracts/interfaces/IDistributor.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.5;
 
 interface IDistributor {
-    function distribute() external;
+    function distribute() external returns (uint256);
     function bounty() external view returns (uint256);
     function nextRewardAt(uint256 _rate) external view returns (uint256);
     function nextRewardFor(address _recipient) external view returns (uint256);
