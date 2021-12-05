44a40d2b32b383c3029cd8b83853899c32871c80
olyzeus <77356025+olyzeus@users.noreply.github.com>
2021-12-05 16:15:02 -0500

    give rewards to LP stakers with multiple reward tokens

--- test/utils/impersonate_account.js
+++
@@ -1,8 +0,0 @@
-const {network} = require("hardhat");
-
-module.exports = async (address) => {
-    return network.provider.request({
-        method: "hardhat_impersonateAccount",
-        params: [address]
-    });
-}
