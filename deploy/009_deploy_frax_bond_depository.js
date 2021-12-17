afcab379daa74182ed5d19a711b851d323b902e3
0xkowloon <0xkowloon@protonmail.com>
2021-12-17 12:56:47 +0000

    feat: Frax bond terms
    - copied from https://etherscan.io/tx/0xc83d9c015dcc177284a919d7ac5a53e3bf8788ff9e940b294b58150c53674e17

--- deploy/009_deploy_frax_bond_depository.js
+++ deploy/009_deploy_frax_bond_depository.js
@@ -54,7 +54,35 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const stakingHelperArtifact = await get('StakingHelper');
   await fraxBond.setStaking(stakingHelperArtifact.address, true);
 
-  // TODO:
-  // await fraxBond.initializeBondTerms(fraxBondBCV, bondVestingLength, minBondPrice, maxBondPayout, bondFee, maxBondDebt, intialBondDebt);
+  // TODO: Just copying params from
+  // https://etherscan.io/tx/0xc83d9c015dcc177284a919d7ac5a53e3bf8788ff9e940b294b58150c53674e17 (Frax V1 bonds initializeBondTerms)
+  // for now. Need to adjust at least bondVestingLength, minBondPrice, maxBondPayout.
+  const fraxBondBCV = 300;
+
+  // TODO: this number doesn't matter, we will update the bond depository
+  // contracts to use timestamps instead of block numbers.
+  // Will just get this contract deployed for now.
+  const bondVestingLength = 33110;
+
+  const minBondPrice = 29000;
+
+  // 0.05% of BRICK supply
+  const maxBondPayout = 50
+
+  // bonding fee given to the DAO (100%)
+  const bondFee = 10000;
+
+  const maxBondDebt = ethers.utils.parseUnits('700000', 9);
+  const initialBondDebt = 0;
+
+  await fraxBond.initializeBondTerms(
+    fraxBondBCV,
+    bondVestingLength,
+    minBondPrice,
+    maxBondPayout,
+    bondFee,
+    maxBondDebt,
+    initialBondDebt,
+  );
 };
 module.exports.tags = ['Staking', 'AllEnvironments'];
