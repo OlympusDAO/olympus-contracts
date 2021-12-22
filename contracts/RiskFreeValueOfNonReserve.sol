6038795c8e433aeefdb8f376ca00fc0829744b66
JeffX <JeffreyJExtor@gmail.com>
2021-12-22 12:08:39 -0500

    Small contract that we can use in place of liquidity token calculator for non reserve tokens. This will value the tokens at 0 so they do not remove any reserves when managing

---
+++ contracts/RiskFreeValueOfNonReserve.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+contract RiskFreeValueOfNonReserve {
+    function valuation(address token_, uint amount_) external view returns( uint _value ) {}
+}
