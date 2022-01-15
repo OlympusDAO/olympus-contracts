cfed12b834b85c4b4a7b366deeea864f49f574ba
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-01-15 23:33:18 +0100

    feat: BaseAllocator and TreasuryExtender done

--- contracts/TreasuryExtender.sol
+++ contracts/TreasuryExtender.sol
@@ -12,8 +12,6 @@ import "./types/OlympusAccessControlledImproved.sol";
 // libraries
 import "./libraries/SafeERC20.sol";
 
-// will get interface because allocators will need it
-// TODO: not finished
 contract TreasuryExtender is OlympusAccessControlledImproved, ITreasuryExtender {
     using SafeERC20 for IERC20;
 
@@ -23,7 +21,6 @@ contract TreasuryExtender is OlympusAccessControlledImproved, ITreasuryExtender 
 
     mapping(IAllocator => AllocatorData) public allocatorData;
 
-    uint256 private totalAmountAllocated;
     uint256 private totalValueAllocated;
 
     constructor(address treasuryAddress, address authorityAddress)
@@ -38,107 +35,231 @@ contract TreasuryExtender is OlympusAccessControlledImproved, ITreasuryExtender 
         require(AllocatorStatus.ACTIVATED == status, "TreasuryExtender::AllocatorOffline");
     }
 
+    function _onlyAllocator(uint256 id, address sender) internal view {
+        require(IAllocator(sender) == allocators[id], "TreasuryExtender::OnlyAllocator");
+    }
+
     //// FUNCTIONS
 
-    // TODO: needs checks
     function registerAllocator(address newAllocatorAddress) external override {
-        _onlyGuardian();
+        // reads
         IAllocator allocator = IAllocator(newAllocatorAddress);
+
+        // checks
+        _onlyGuardian();
+        require(allocator.id() == 0, "TreasuryExtender::AllocatorRegistered");
+
+        // effects
         allocators.push(allocator);
+
+        // interactions
+        allocator.setId(allocators.length - 1);
+
+        // events
+        emit NewAllocatorRegistered(newAllocatorAddress, allocator.getToken(), allocator.id());
     }
 
-    function setAllocationLimit(address allocatorAddress, uint256 limit) external override {
-        _onlyGuardian();
+    function setAllocatorLimits(address allocatorAddress, AllocatorLimits memory limits) external override {
+        // reads
         IAllocator allocator = IAllocator(allocatorAddress);
-        _setAllocationLimit(allocator, limit);
+
+        // interactions
+        _setAllocatorLimits(allocator, limits);
     }
 
-    function setAllocationLimit(uint256 allocatorId, uint256 limit) external override {
-        _onlyGuardian();
-        IAllocator allocator = allocators[allocatorId];
-        _setAllocationLimit(allocator, limit);
+    function setAllocatorLimits(uint256 id, AllocatorLimits memory limits) external override {
+        // reads
+        IAllocator allocator = allocators[id];
+
+        // interactions
+        _setAllocatorLimits(allocator, limits);
     }
 
-    function requestFundsFromTreasury(uint256 allocatorId, uint256 amount) external override {
-        _onlyGuardian();
-        _requestFundsFromTreasury(allocators[allocatorId], amount);
+    // either gain is in allocated or we can just fetch it via balances
+    // so either gain > loss, loss == 0,
+    // or gain < loss, gain == 0
+    // or gain == 0 && loss == 0
+    function report(
+        uint256 id,
+        uint128 gain,
+        uint128 loss
+    ) external override {
+        // check before reading because might be common in some
+        if (gain == 0 && loss == 0) return;
+
+        // reads
+        IAllocator allocator = allocators[id];
+        AllocatorPerformance memory perf = allocatorData[allocator].performance;
+
+        // checks
+        _onlyAllocator(id, msg.sender);
+        _allocatorActivated(allocator.status());
+
+        // effect
+        perf.gain += gain;
+        perf.loss += loss;
+
+        allocatorData[allocator].performance = perf;
+
+        if (loss != 0) allocatorData[allocator].holdings.allocated -= loss;
+
+        if (gain > loss) emit AllocatorReportedGain(id, gain);
+        else emit AllocatorReportedLoss(id, loss);
+    }
+
+    function requestFundsFromTreasury(uint256 id, uint256 amount) external override {
+        _requestFundsFromTreasury(allocators[id], amount);
     }
 
     function requestFundsFromTreasury(address allocatorAddress, uint256 amount) external override {
-        _onlyGuardian();
         _requestFundsFromTreasury(IAllocator(allocatorAddress), amount);
     }
 
-    function returnFundsToTreasury(uint256 allocatorId, uint256 amount) external override {
-        _onlyGuardian();
-        _returnFundsToTreasury(allocators[allocatorId], amount);
+    function returnFundsToTreasury(uint256 id, uint256 amount) external override {
+        _returnFundsToTreasury(allocators[id], amount);
     }
 
     function returnFundsToTreasury(address allocatorAddress, uint256 amount) external override {
-        _onlyGuardian();
         _returnFundsToTreasury(IAllocator(allocatorAddress), amount);
     }
 
-    function getAllocatorByID(uint256 allocatorId) external view override returns (address allocatorAddress) {
-        allocatorAddress = address(allocators[allocatorId]);
+    function returnRewardsToTreasury(
+        uint256 id,
+        address token,
+        uint256 amount
+    ) external {
+        _returnRewardsToTreasury(allocators[id], IERC20(token), amount);
     }
 
-    function getTotalAmountAllocated() external view override returns (uint256) {
-        return totalAmountAllocated;
+    function returnRewardsToTreasury(
+        address allocatorAddress,
+        address token,
+        uint256 amount
+    ) external {
+        _returnRewardsToTreasury(IAllocator(allocatorAddress), IERC20(token), amount);
+    }
+
+    function getAllocatorByID(uint256 id) external view override returns (address allocatorAddress) {
+        allocatorAddress = address(allocators[id]);
     }
 
     function getTotalValueAllocated() external view override returns (uint256) {
         return totalValueAllocated;
     }
 
-    function _setAllocationLimit(IAllocator allocator, uint256 limit) internal {
-        allocatorData[allocator].maxAmountAllocated = limit;
+    function getAllocatorLimits(uint256 id) external view override returns (AllocatorLimits memory) {
+        return allocatorData[allocators[id]].limits;
+    }
+
+    function getAllocatorPerformance(uint256 id) external view override returns (AllocatorPerformance memory) {
+        return allocatorData[allocators[id]].performance;
+    }
+
+    function getAllocatorAllocated(uint256 id) external view override returns (uint256) {
+        return allocatorData[allocators[id]].holdings.allocated;
+    }
+
+    function _setAllocatorLimits(IAllocator allocator, AllocatorLimits memory limits) internal {
+        // checks
+        _onlyGuardian();
+        _allocatorActivated(allocator.status());
+
+        // effects
+        allocatorData[allocator].limits = limits;
+
+        // events
+        emit AllocatorLimitsChanged(allocator.id(), limits.allocated, limits.loss);
     }
 
     function _requestFundsFromTreasury(IAllocator allocator, uint256 amount) internal {
         // reads
         AllocatorData memory data = allocatorData[allocator];
-        address tokenAllocated = allocator.getTokenAllocated();
+        address tokenAllocated = allocator.getToken();
         uint256 value = treasury.tokenValue(tokenAllocated, amount);
 
         // checks
+        _onlyGuardian();
         _allocatorActivated(allocator.status());
+        _allocatorBelowLimit(data, amount);
 
         // interaction (withdrawing)
         treasury.manage(tokenAllocated, amount);
 
         // effects
-        totalAmountAllocated += amount;
         totalValueAllocated += value;
-
-        data.amountAllocated += amount;
-        data.valueAllocated += value;
-
-        allocatorData[allocator] = data;
+        allocatorData[allocator].holdings.allocated += amount;
 
         // interaction (depositing)
         IERC20(tokenAllocated).safeTransfer(address(allocator), amount);
+
+        // events
+        emit AllocatorFunded(allocator.id(), amount, value);
     }
 
     function _returnFundsToTreasury(IAllocator allocator, uint256 amount) internal {
         // reads
-        AllocatorData memory data = allocatorData[allocator];
-        address tokenAllocated = allocator.getTokenAllocated();
+        uint256 allocated = allocatorData[allocator].holdings.allocated;
+        uint128 gain = allocatorData[allocator].performance.gain;
+
+        if (amount > allocated) {
+            amount -= allocated;
+            if (amount > gain) {
+                amount = allocated + gain;
+                gain = 0;
+            } else {
+                // yes, amount should never > gain, we have safemath
+                gain -= uint128(amount);
+                amount += allocated;
+            }
+            allocated = 0;
+        } else {
+            allocated -= amount;
+        }
+
+        address tokenAllocated = allocator.getToken();
         uint256 value = treasury.tokenValue(tokenAllocated, amount);
+
+        // checks
+        _onlyGuardian();
 
         // interaction (withdrawing)
         IERC20(tokenAllocated).safeTransferFrom(address(allocator), address(this), amount);
 
         // effects
-        totalAmountAllocated -= amount;
         totalValueAllocated -= value;
-
-        data.amountAllocated -= amount;
-        data.valueAllocated -= value;
-
-        allocatorData[allocator] = data;
+        allocatorData[allocator].holdings.allocated = allocated;
+        if (allocated == 0) allocatorData[allocator].performance.gain = gain;
 
         // interaction (depositing)
         assert(treasury.deposit(amount, tokenAllocated, value) == 0);
+
+        // events
+        emit AllocatorWithdrawal(allocator.id(), amount, value);
+    }
+
+    function _returnRewardsToTreasury(
+        IAllocator allocator,
+        IERC20 token,
+        uint256 amount
+    ) internal {
+        // reads
+        uint256 balance = token.balanceOf(address(allocator));
+        amount = (balance < amount) ? balance : amount;
+        uint256 value = treasury.tokenValue(address(token), amount);
+
+        // checks
+        _onlyGuardian();
+
+        // interactions
+        token.safeTransferFrom(address(allocator), address(this), amount);
+
+        assert(treasury.deposit(amount, address(token), value) == 0);
+
+        // events
+        emit AllocatorRewardsWithdrawal(allocator.id(), amount, value);
+    }
+
+    function _allocatorBelowLimit(AllocatorData memory data, uint256 amount) internal pure {
+        require(data.holdings.allocated + amount <= data.limits.allocated, "TreasuryExtender::MaxAllocation");
     }
 }
--- contracts/interfaces/ITreasuryExtender.sol
+++ contracts/interfaces/ITreasuryExtender.sol
@@ -1,26 +1,51 @@
 pragma solidity ^0.8.10;
 
+struct AllocatorPerformance {
+    uint128 gain;
+    uint128 loss;
+}
+
+struct AllocatorLimits {
+    uint128 allocated;
+    uint128 loss;
+}
+
+struct AllocatorHoldings {
+    uint256 allocated;
+}
+
 struct AllocatorData {
-    uint256 amountAllocated;
-    uint256 valueAllocated;
-    uint256 maxAmountAllocated;
+    AllocatorHoldings holdings;
+    AllocatorLimits limits;
+    AllocatorPerformance performance;
 }
 
 interface ITreasuryExtender {
-    // TODO: throw events into functions
     event NewAllocatorRegistered(address allocatorAddress, address allocatorToken, uint256 allocatorId);
 
-    event AllocatorFunded(address allocatorAddress, uint256 amount, uint256 value);
+    event AllocatorFunded(uint256 allocatorId, uint256 amount, uint256 value);
 
-    event AllocatorWithdrawal(address allocatorAddress, uint256 amount, uint256 value);
+    event AllocatorWithdrawal(uint256 allocatorId, uint256 amount, uint256 value);
 
-    event AllocationLimitChanged(address allocatorAddress, uint256 amount);
+    event AllocatorRewardsWithdrawal(uint256 allocatorId, uint256 amount, uint256 value);
+
+    event AllocatorReportedGain(uint256 allocatorId, uint128 gain);
+
+    event AllocatorReportedLoss(uint256 allocatorId, uint128 loss);
+
+    event AllocatorLimitsChanged(uint256 allocatorId, uint128 allocationLimit, uint128 lossLimit);
 
     function registerAllocator(address newAllocatorAddress) external;
 
-    function setAllocationLimit(address allocatorAddress, uint256 limit) external;
+    function setAllocatorLimits(address allocatorAddress, AllocatorLimits memory limits) external;
 
-    function setAllocationLimit(uint256 allocatorId, uint256 limit) external;
+    function setAllocatorLimits(uint256 allocatorId, AllocatorLimits memory limits) external;
+
+    function report(
+        uint256 id,
+        uint128 gain,
+        uint128 loss
+    ) external;
 
     function requestFundsFromTreasury(uint256 allocatorId, uint256 amount) external;
 
@@ -30,9 +55,25 @@ interface ITreasuryExtender {
 
     function returnFundsToTreasury(address allocatorAddress, uint256 amount) external;
 
+    function returnRewardsToTreasury(
+        uint256 allocatorId,
+        address token,
+        uint256 amount
+    ) external;
+
+    function returnRewardsToTreasury(
+        address allocatorAddress,
+        address token,
+        uint256 amount
+    ) external;
+
     function getAllocatorByID(uint256 allocatorId) external view returns (address);
 
-    function getTotalAmountAllocated() external view returns (uint256);
-
     function getTotalValueAllocated() external view returns (uint256);
+
+    function getAllocatorAllocated(uint256 id) external view returns (uint256);
+
+    function getAllocatorLimits(uint256 id) external view returns (AllocatorLimits memory);
+
+    function getAllocatorPerformance(uint256 id) external view returns (AllocatorPerformance memory);
 }
