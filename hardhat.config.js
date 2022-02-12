52844502bafd350fe9fa594d5e31d6e62332e4d4
Mohammad Rayed <mohammad.rayed@northsouth.edu>
2022-02-13 03:54:15 +0600

    Setup Fantom testnet for hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+let secret = require("./secret")
+
 module.exports = {
   solidity: "0.7.5",
+  networks: {
+    fantom: {
+      url: secret.url,
+      accounts: [secret.key]
+    }
+  }
 };
