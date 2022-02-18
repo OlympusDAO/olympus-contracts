9730035ab43874c22aad2b32ff37ddea38160cc2
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-18 18:43:14 +0100

    feat: add more coins to utils

--- test/utils/coins.ts
+++ test/utils/coins.ts
@@ -1,11 +1,20 @@
+// CORE
 const ohm = "0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5";
 const gohm = "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f";
 const sohm = "0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F";
+const weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+// STABLES
 const dai = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 const usdc = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 const usdt = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 const mim = "0x99D8a9C45b2ecA8864373A26D1459e3Dff1e17F3";
-const weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 const frax = "0x853d955aCEf822Db058eb8505911ED77F175b99e";
 
-export const coins = { ohm, gohm, sohm, dai, usdc, usdt, mim, weth, frax };
+// AAVE
+const adai = "0x028171bCA77440897B824Ca71D1c56caC55b68A3";
+const aweth = "0x030bA81f1c18d280636F32af80b9AAd02Cf0854e";
+const ausdc = "0xBcca60bB61934080951369a648Fb03DF4F96263C";
+const afrax = "0xd4937682df3C8aEF4FE912A96A74121C0829E664";
+
+export const coins = { ohm, gohm, sohm, dai, usdc, usdt, mim, weth, frax, adai, aweth, ausdc, afrax };
