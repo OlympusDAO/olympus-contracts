93efb3cbb36e5619204dd1a38ef5ef8f4ffb4169
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-21 18:18:02 +0100

    feat: continue writing Convex allocator, add interfaces separately for reuse

---
+++ contracts/allocators/interfaces/ConvexInterfaces.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.0;
+
+interface IConvex {
+    function deposit(
+        uint256 _pid,
+        uint256 _amount,
+        bool _stake
+    ) external returns (bool);
+
+    function depositAll(uint256 _pid, bool _stake) external returns (bool);
+}
+
+interface IConvexRewards {
+    function withdrawAndUnwrap(uint256 _amount, bool _claim) external returns (bool);
+
+    function getReward() external returns (bool);
+
+    function earned(address _account) external view returns (uint256);
+}
