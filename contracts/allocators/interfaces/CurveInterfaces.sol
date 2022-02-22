04c5e4a5c7896cd621756f2c8660b7712154e6d8
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-22 17:01:41 +0100

    feat: almost down with convex allocator

--- contracts/allocators/interfaces/CurveInterfaces.sol
+++ contracts/allocators/interfaces/CurveInterfaces.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.0;
 
 interface ICurveAddressProvider {
@@ -31,13 +32,24 @@ interface ICurveDepositZap {
         address receiver
     ) external returns (uint256);
 
+    function remove_liquidity_one_coin(
+        address _pool,
+        uint256 _burn_amount,
+        int128 i,
+        uint256 _min_amount
+    ) external returns (uint256);
+
     function calc_token_amount(
         address _pool,
         uint256[4] memory _amounts,
         bool _is_deposit
     ) external view returns (uint256);
+
+    function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
 }
 
 interface ICurvePool {
     function coins(uint256 i) external view returns (address);
+
+    function calc_withdraw_one_coin(uint256 _token_amount, int128 i) external view returns (uint256);
 }
