b7984e95bd6a1150c3b81be0e235ad7391caecd0
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-22 17:01:41 +0100

    feat: almost down with convex allocator

--- contracts/allocators/ConvexAllocatorV2.sol
+++ contracts/allocators/ConvexAllocatorV2.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.10;
 
 // types
@@ -7,16 +8,27 @@ import "../types/BaseAllocator.sol";
 import "./interfaces/CurveInterfaces.sol";
 import "./interfaces/ConvexInterfaces.sol";
 
-struct AllocatorTargetData {
+struct ConvexTargetData {
     IConvexRewards pool;
-    uint96 index;
+    IERC20 lp;
+    uint96 pid;
+}
+
+struct CurveTargetData {
+    IERC20 lp;
+    // coin id
+    uint96 cid;
+}
+
+struct AllocatorTargetData {
+    ConvexTargetData convex;
+    CurveTargetData curve;
 }
 
 struct NewDepositData {
     AllocatorTargetData targets;
-    IERC20 token;
-    IERC20 lpToken;
-    IERC20[] rewardTokens;
+    // 0 is underlying and rest are reward tokens
+    IERC20[] tokens;
 }
 
 contract CurveConvexAllocator is BaseAllocator {
@@ -33,7 +45,6 @@ contract CurveConvexAllocator is BaseAllocator {
     IConvex internal immutable _booster = IConvex(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
 
     // other
-    IERC20[] internal _lpTokens;
     IERC20[] internal _rewardTokens;
 
     AllocatorTargetData[] internal _targets;
@@ -44,30 +55,103 @@ contract CurveConvexAllocator is BaseAllocator {
     function _update(uint256 id) internal override returns (uint128 gain, uint128 loss) {
         // reads
         uint256 index = tokenIds[id];
+
+        AllocatorTargetData memory target = _targets[index];
+
+        IERC20 lpCurve = target.curve.lp;
+        IERC20 underlying = _tokens[index];
+
+        address pool = _registry.get_pool_from_lp_token(address(lpCurve));
+
         uint256[4] memory fixedInputArray;
-        fixedInputArray[_targets[index].index] = _tokens[index].balanceOf(address(this));
+        uint256 depositAmount = underlying.balanceOf(address(this));
+        fixedInputArray[target.curve.cid] = depositAmount;
 
         // checks
         _onlyGuardian();
 
         // interactions
-        _zapIntoCurve(_registry.get_pool_from_lp_token(address(_lpTokens[index])), fixedInputArray);
+        underlying.approve(address(_zap3), depositAmount);
+        depositAmount = _zapIntoCurve(pool, fixedInputArray);
+
+        lpCurve.approve(address(_booster), depositAmount);
+        _booster.depositAll(target.convex.pid, true);
+
+        // effects
+        uint256 received = _amountAllocated(ICurvePool(pool), _booster, target.convex.pid, target.curve.cid);
+        uint256 last = extender.getAllocatorAllocated(id) + extender.getAllocatorPerformance(id).gain;
+
+        if (received >= last) gain = uint128(received - last);
+        else loss = uint128(last - received);
     }
 
-    function deallocate(uint256[] memory amounts) public override {}
+    function deallocate(uint256[] memory amounts) public override {
+        // reads
+        uint256[4] memory fixedInputArray;
+        uint256 amount;
+        uint256 lpCurveChange;
+
+        // checks
+        _onlyGuardian();
+
+        for (uint256 index; index < amounts.length; index++) {
+            amount = amounts[index];
+
+            if (amount > 0) {
+                // only now do we really start
+                // reads
+                AllocatorTargetData memory target = _targets[index];
+                IERC20 lpCurve = target.curve.lp;
+                address curvePool = _registry.get_pool_from_lp_token(address(lpCurve));
+
+                fixedInputArray[target.curve.cid] = amount;
+                lpCurveChange = _zap3.calc_token_amount(curvePool, fixedInputArray, false);
+
+                // interactions
+                // check if approve necessary
+                target.convex.pool.withdrawAndUnwrap(lpCurveChange, false);
+
+                lpCurve.approve(address(_zap3), lpCurveChange);
+                _zap3.remove_liquidity_one_coin(
+                    curvePool,
+                    lpCurveChange,
+                    int128(int96(target.curve.cid)),
+                    (amount * slippage) / 1e18
+                );
+            }
+        }
+    }
 
     function _deactivate(bool panic) internal override {}
 
     function _prepareMigration() internal override {}
 
-    function amountAllocated(uint256 id) public view override returns (uint256) {}
+    function amountAllocated(uint256 id) public view override returns (uint256) {
+        uint256 index = tokenIds[id];
+        AllocatorTargetData memory target = _targets[index];
+        return
+            _amountAllocated(
+                ICurvePool(_registry.get_pool_from_lp_token(address(target.curve.lp))),
+                _booster,
+                target.convex.pid,
+                target.curve.cid
+            );
+    }
 
     function rewardTokens() public view override returns (IERC20[] memory) {
         return _rewardTokens;
     }
 
     function utilityTokens() public view override returns (IERC20[] memory) {
-        return _lpTokens;
+        uint256 length = _targets.length;
+
+        IERC20[] memory tokensUtility = new IERC20[](length);
+
+        for (uint256 i; i < length; i++) {
+            tokensUtility[i] = _targets[i].convex.lp;
+        }
+
+        return tokensUtility;
     }
 
     function name() external pure override returns (string memory) {
@@ -81,25 +165,24 @@ contract CurveConvexAllocator is BaseAllocator {
     // specific
     function addDeposit(NewDepositData calldata data) external {
         // reads
-        uint256 length = data.rewardTokens.length;
+        uint256 length = data.tokens.length;
 
         // checks
         _onlyGuardian();
 
         // interactions
-        data.token.approve(address(extender), type(uint256).max);
-        data.lpToken.approve(address(extender), type(uint256).max);
+        data.tokens[0].approve(address(extender), type(uint256).max);
+        data.targets.curve.lp.approve(address(extender), type(uint256).max);
+        data.targets.convex.lp.approve(address(extender), type(uint256).max);
 
         // effects
         _targets.push(data.targets);
-
-        _tokens.push(data.token);
-        _lpTokens.push(data.lpToken);
+        _tokens.push(data.tokens[0]);
 
         if (length > 0) {
-            for (uint256 i; i < length; i++) {
+            for (uint256 i = 1; i < length; i++) {
                 // read
-                IERC20 rewardToken = data.rewardTokens[i];
+                IERC20 rewardToken = data.tokens[i];
 
                 // interaction
                 rewardToken.approve(address(extender), type(uint256).max);
@@ -116,9 +199,9 @@ contract CurveConvexAllocator is BaseAllocator {
         AllocatorTargetData calldata target
     ) external {
         // reads
-        uint256[] memory inputArray = new uint256[](id + 1);
-        uint256[4] memory fixedInputArray;
         uint256 index = tokenIds[id];
+        uint256[] memory inputArray = new uint256[](index + 1);
+        uint256[4] memory fixedInputArray;
 
         inputArray[index] = amountAllocated(id);
 
@@ -128,7 +211,7 @@ contract CurveConvexAllocator is BaseAllocator {
         // effects + interactions
         deallocate(inputArray);
 
-        fixedInputArray[target.index] = _tokens[index].balanceOf(address(this));
+        fixedInputArray[target.curve.cid] = _tokens[index].balanceOf(address(this));
 
         _zapIntoCurve(_registry.get_pool_from_lp_token(address(newLpToken)), fixedInputArray);
 
@@ -142,5 +225,23 @@ contract CurveConvexAllocator is BaseAllocator {
 
     function _zapIntoCurve(address pool, uint256[4] memory amounts) internal returns (uint256) {
         return _zap3.add_liquidity(pool, amounts, (_zap3.calc_token_amount(pool, amounts, true) * slippage) / 1e18);
+    }
+
+    function _amountAllocated(
+        ICurvePool pool,
+        IConvex booster,
+        uint256 pidConvex,
+        uint256 cidCurve
+    ) internal view returns (uint256) {
+        return
+            pool.calc_withdraw_one_coin(
+                _getConvexRewardPool(booster, pidConvex).balanceOf(address(this)),
+                int128(int256(cidCurve))
+            );
+    }
+
+    function _getConvexRewardPool(IConvex booster, uint256 pid) internal view returns (IConvexRewards) {
+        (, , , address rewards, , ) = booster.poolInfo(pid);
+        return IConvexRewards(rewards);
     }
 }
--- contracts/allocators/interfaces/ConvexInterfaces.sol
+++ contracts/allocators/interfaces/ConvexInterfaces.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.0;
 
 interface IConvex {
@@ -8,12 +9,26 @@ interface IConvex {
     ) external returns (bool);
 
     function depositAll(uint256 _pid, bool _stake) external returns (bool);
+
+    function poolInfo(uint256)
+        external
+        view
+        returns (
+            address,
+            address,
+            address,
+            address,
+            address,
+            bool
+        );
 }
 
 interface IConvexRewards {
     function withdrawAndUnwrap(uint256 _amount, bool _claim) external returns (bool);
 
     function getReward() external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
 
     function earned(address _account) external view returns (uint256);
 }
