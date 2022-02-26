c57160271024a2080f438c6e45d8bc008f64a7fe
mirru2532 <kirrukirruheroku5@protonmail.com>
2022-02-26 02:10:54 +0100

    style: TER-02C: Inexistent Visibility Specifier

--- contracts/TreasuryExtender.sol
+++ contracts/TreasuryExtender.sol
@@ -48,7 +48,7 @@ contract TreasuryExtender is OlympusAccessControlledV2, ITreasuryExtender {
     using SafeERC20 for IERC20;
 
     // The Olympus Treasury.
-    ITreasury immutable treasury;
+    ITreasury immutable public treasury;
 
     // Enumerable Allocators according to deposit IDs.
     /// @dev NOTE: Allocator enumeration starts from index 1.
