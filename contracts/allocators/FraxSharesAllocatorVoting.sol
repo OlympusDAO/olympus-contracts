98de82a7efdcb7a0e12772585bc012ab933d3534
PARABRAHMAN0 <noreply@ignore.com>
2022-04-08 14:11:00 +0000

    wtg

---
+++ contracts/allocators/FraxSharesAllocatorVoting.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.7.5;
+
+import "./FraxSharesAllocator.sol";
+import "./interfaces/FxsInterfaces.sol";
+
+contract FraxSharesAllocatorVoting is FraxSharesAllocator {
+    GaugeController public gaugeController;
+
+    function setGaugeController(address _gaugeController) external onlyOwner {
+        gaugeController = GaugeController(_gaugeController);
+    }
+
+    function vote(address gauge, uint256 votingPowerToUse) external onlyOwner {
+        gaugeController.vote_for_gauge_weights(gauge, votingPowerToUse);
+    }
+}
