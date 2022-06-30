2ec526fbf0f6c3145493ccf2a1a9f3baaa009de7
Lienid <0xLienid@protonmail.com>
2022-06-30 16:19:53 -0400

    Update for testnet deployment

--- scripts/deployIncurDebt.ts
+++ scripts/deployIncurDebt.ts
@@ -6,31 +6,32 @@ import { BalancerStrategy__factory } from "../types";
 
 async function main() {
     // Mainnet
-    const ohm = "0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5";
-    const treasury = "0x9A315BdF513367C0377FB36545857d12e85813Ef";
-    const sohm = "0x04906695D6D12CF5459975d7C3C03356E4Ccd460";
-    const gohm = "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f";
-    const staking = "0xB63cac384247597756545b500253ff8E607a8020";
-    const authority = "0x1c21f8ea7e39e2ba00bc12d2968d63f4acb38b7a";
-    const curvePoolFactory = "0xB9fC157394Af804a3578134A6585C0dc9cc990d4";
-    const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-    const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
-    const sushiRouter = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f";
-    const sushiFactory = "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac";
-    const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
+    // const ohm = "0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5";
+    // const treasury = "0x9A315BdF513367C0377FB36545857d12e85813Ef";
+    // const sohm = "0x04906695D6D12CF5459975d7C3C03356E4Ccd460";
+    // const gohm = "0x0ab87046fBb341D058F17CBC4c1133F25a20a52f";
+    // const staking = "0xB63cac384247597756545b500253ff8E607a8020";
+    // const authority = "0x1c21f8ea7e39e2ba00bc12d2968d63f4acb38b7a";
+    // const curvePoolFactory = "0xB9fC157394Af804a3578134A6585C0dc9cc990d4";
+    // const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    // const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    // const sushiRouter = "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f";
+    // const sushiFactory = "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac";
+    // const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
 
     // Testnet
-    // const ohm = "0x10b27a31AA4d7544F89898ccAf3Faf776F5671C4"
-    // const treasury = "0x0b28Da6b497c984ed48b0d69f1DF4010071fC78e"
-    // const sohm = "0xebED323CEbe4FfF65F7D7612Ea04313F718E5A75"
-    // const gohm = "0xcF2D6893A1CB459fD6B48dC9C41c6110B968611E"
-    // const staking = "0x06984c3A9EB8e3A8df02A4C09770D5886185792D"
-    // const authority = "0x4208befd8f546282ab43a30085774513227b656c"
-    // const curvePoolFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-    // const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-    // const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-    // const sushiRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-    // const sushiFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+    const ohm = "0x0595328847AF962F951a4f8F8eE9A3Bf261e4f6b"
+    const treasury = "0xB3e1dF7951a62fFb5eF7D3b1C9D80CF09325580A"
+    const sohm = "0x4EFe119F4949319f2Acb12efD615a7B63896482B"
+    const gohm = "0xC1863141dc1861122d5410fB5973951c82871d98"
+    const staking = "0x7263372b9ff6E619d8774aEB046cE313677E2Ec7"
+    const authority = "0x4A8c9502A34962a2C6d73c5D181dAaeF3dcDc88D"
+    const curvePoolFactory = "0x4d22B8446e68eD3BB425CFb35e2aBC2ddc24ae77" // random curve pool (not sure if legit)
+    const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+    const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+    const sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"
+    const sushiFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
+    const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
 
     const incurDebtFactory = (await ethers.getContractFactory("IncurDebt")) as IncurDebt__factory;
     const incurDebt = await incurDebtFactory.deploy(ohm, gohm, sohm, staking, treasury, authority);
