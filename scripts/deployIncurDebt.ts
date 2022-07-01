0072c59481794714b7176366ec0500f7b6aaad0a
Lienid <0xLienid@protonmail.com>
2022-07-01 10:32:41 -0400

    Merge off IncurDebt, add encoding functions to interface

--- scripts/deployIncurDebt.ts
+++ scripts/deployIncurDebt.ts
@@ -20,17 +20,17 @@ async function main() {
     // const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
 
     // Testnet
-    const ohm = "0x0595328847AF962F951a4f8F8eE9A3Bf261e4f6b"
-    const treasury = "0xB3e1dF7951a62fFb5eF7D3b1C9D80CF09325580A"
-    const sohm = "0x4EFe119F4949319f2Acb12efD615a7B63896482B"
-    const gohm = "0xC1863141dc1861122d5410fB5973951c82871d98"
-    const staking = "0x7263372b9ff6E619d8774aEB046cE313677E2Ec7"
-    const authority = "0x4A8c9502A34962a2C6d73c5D181dAaeF3dcDc88D"
-    const curvePoolFactory = "0x4d22B8446e68eD3BB425CFb35e2aBC2ddc24ae77" // random curve pool (not sure if legit)
-    const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
-    const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
-    const sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"
-    const sushiFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4"
+    const ohm = "0x0595328847AF962F951a4f8F8eE9A3Bf261e4f6b";
+    const treasury = "0xB3e1dF7951a62fFb5eF7D3b1C9D80CF09325580A";
+    const sohm = "0x4EFe119F4949319f2Acb12efD615a7B63896482B";
+    const gohm = "0xC1863141dc1861122d5410fB5973951c82871d98";
+    const staking = "0x7263372b9ff6E619d8774aEB046cE313677E2Ec7";
+    const authority = "0x4A8c9502A34962a2C6d73c5D181dAaeF3dcDc88D";
+    const curvePoolFactory = "0x4d22B8446e68eD3BB425CFb35e2aBC2ddc24ae77"; // random curve pool (not sure if legit)
+    const uniswapRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+    const uniswapFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    const sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506";
+    const sushiFactory = "0xc35DADB65012eC5796536bD9864eD8773aBc74C4";
     const balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
 
     const incurDebtFactory = (await ethers.getContractFactory("IncurDebt")) as IncurDebt__factory;
